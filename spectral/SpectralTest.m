1945cf1d13001c3e94f1364d6aef5e89df46dd83
David Brainard <brainard@psych.upenn.edu>
2020-05-16 21:42:49 -0400

    Working on spectral matching receptor isolate

--- spectral/SpectralTest.m
+++ spectral/SpectralTest.m
@@ -8,13 +8,16 @@
 clear; close all;
 
 %% Generate a OneLight cal file
-S = [400 2 151];
+S = [380 2 201];
 cal = sssSpoofOneLightCal('S',S, ....
     'plotBasis',true,...
     'gaussianPeakWls',[437 485 540 562 585 618 652], ...
     'gaussianFWHM',25);
 S = cal.describe.S;
 wls = SToWls(S);
+lowProjectWl = 420;
+highProjectWl = 680;
+projectIndices = find(wls > lowProjectWl & wls < highProjectWl);
 
 %% Cone fundamentals
 psiParamsStruct.coneParams = DefaultConeParams('cie_asano');
@@ -45,47 +48,108 @@ plot(wls,halfOnSpdChk,'k','LineWidth',1);
 %
 % Generate a black body radiator and put it into right general scale
 theTemp = 6500;
-theSpd = GenerateBlackBody(theTemp,wls);
-theSpd = 0.6*sum(halfOnSpd)*theSpd/sum(theSpd);
+theIsolatingNaturalSpd = GenerateBlackBody(theTemp,wls);
+theIsolatingNaturalSpd = 0.6*sum(halfOnSpd)*theIsolatingNaturalSpd/sum(theIsolatingNaturalSpd);
 
 % Use OL machinery to get primaries and then the spd
-thePrimaries = OLSpdToPrimary(cal,theSpd,'lambda',0.01);
-theFitSpd = OLPrimaryToSpd(cal,thePrimaries);
+theIsolatingNaturalPrimaries = OLSpdToPrimary(cal,theIsolatingNaturalSpd,'lambda',0.01);
+theFitSpd = OLPrimaryToSpd(cal,theIsolatingNaturalPrimaries);
 
 % Use non-neg least squares for comparison
-weights = lsqnonneg(cal.computed.pr650M,theSpd);
+weights = lsqnonneg(cal.computed.pr650M,theIsolatingNaturalSpd);
 theFitSpdRegress = cal.computed.pr650M*weights;
 
 % Plot
 figure; clf; hold on
-plot(wls,theSpd,'k','LineWidth',3);
+plot(wls,theIsolatingNaturalSpd,'k','LineWidth',3);
 plot(wls,theFitSpd,'r','LineWidth',2);
 plot(wls,theFitSpdRegress,'g','LineWidth',1);
 
 %% Generate cone isolating spectral modulations with a simple basis
 load B_cieday
-theBasis = SplineSpd(S_cieday,B_cieday,S);
-M_PrimariesToCones = Tcones*theBasis;
-M_ConesToPrimaries = inv(M_PrimariesToCones);
+theNaturalBasis = SplineSpd(S_cieday,B_cieday,S);
+M_NaturalPrimariesToCones = Tcones*theNaturalBasis;
+M_ConesToNaturalPrimaries = inv(M_NaturalPrimariesToCones);
 
 % Get background within basis
-theBgPrimaries = theBasis\theSpd;
-theBgSpd = theBasis*theBgPrimaries;
-theBgLms = Tcones*theBgSpd;
+theBgNaturalPrimaries = theNaturalBasis\theIsolatingNaturalSpd;
+theBgNaturalSpd = theNaturalBasis*theBgNaturalPrimaries;
+theBgNaturalLms = Tcones*theBgNaturalSpd;
 figure; clf; hold on
-plot(wls,theBgSpd,'b:','LineWidth',2);
+plot(wls,theBgNaturalSpd,'b:','LineWidth',2);
 
 % Define desired cone contrast
 theLmsContrast = [1 -1 0]'/10;
-theLmsInc = theLmsContrast.*theBgLms;
-theLms = theLmsInc+theBgLms;
-thePrimaries = M_ConesToPrimaries*theLms;
-theSpd = theBasis*thePrimaries;
-plot(wls,theSpd,'b','LineWidth',2);
+theLmsInc = theLmsContrast.*theBgNaturalLms;
+theLms = theLmsInc+theBgNaturalLms;
+theIsolatingNaturalPrimaries = M_ConesToNaturalPrimaries*theLms;
+theIsolatingNaturalSpd = theNaturalBasis*theIsolatingNaturalPrimaries;
+plot(wls,theIsolatingNaturalSpd,'b','LineWidth',2);
 
 %% Now we want to find an in-gamut modulation 
+IDENTITY = false;
+if (IDENTITY)
+    B_DevicePrimary = eye(size(cal.computed.pr650M,1));
+    theBgDevicePrimaries = theBgNaturalSpd;
+    initialDevicePrimaries = theIsolatingNaturalSpd;
+else
+    B_DevicePrimary = cal.computed.pr650M;
+    theBgDevicePrimaries = OLSpdToPrimary(cal,theBgNaturalSpd);
+    initialDevicePrimaries = theBgDevicePrimaries;
+end
 
-% modulationBasis = ReceptorIsolate(T_receptors,whichReceptorsToTarget, whichReceptorsToIgnore, whichReceptorsToMinimize, ...
-%     theBasis, backgroundPrimary, backgroundPrimary, whichPrimariesToPin,...
-%     primaryHeadRoom, maxPowerDiff, desiredContrast, ambientSpd);
+whichReceptorsToIsolate = [1 2];
+whichReceptorsToIgnore = [];
+whichReceptorsToZero = setdiff(1:size(Tcones,1),[whichReceptorsToIsolate whichReceptorsToIgnore]);
+desiredContrast = [1 -1]/10;
+
+ambientSpd = cal.computed.pr650MeanDark;
+maxPowerDiff = Inf;
+targetBasis = theNaturalBasis;
+targetLambda = 2;
+primaryHeadRoom = 0;
+
+[isolatingModulationDevicePrimaries] = ReceptorIsolateSpectral(Tcones,whichReceptorsToIsolate, ...
+    whichReceptorsToIgnore,B_DevicePrimary,theBgDevicePrimaries,initialDevicePrimaries, ...
+    primaryHeadRoom,maxPowerDiff,targetBasis,projectIndices,targetLambda,desiredContrast,ambientSpd);
+theBgDeviceSpd = B_DevicePrimary*theBgDevicePrimaries;
+isolatingDeviceUpperPrimaries = isolatingModulationDevicePrimaries + theBgDevicePrimaries;
+isolatingDevicePrimariesLower = -isolatingModulationDevicePrimaries + theBgDevicePrimaries;
+theIsolatingDeviceSpd = B_DevicePrimary*isolatingDeviceUpperPrimaries;
+theBgDeviceLMS = Tcones*theBgDeviceSpd;
+theIsolatingDeviceLMS = Tcones*theIsolatingDeviceSpd;
+theIsolatingContrast = ExcitationsToContrast(theIsolatingDeviceLMS,theBgDeviceLMS);
+fprintf('Desired/obtained contrasts\n');
+for ii = 1:length(whichReceptorsToIsolate)
+    rr = whichReceptorsToIsolate(ii);
+    fprintf('\tReceptor %d (desired/obtained): %0.2f, %0.2f\n',rr,desiredContrast(rr),theIsolatingContrast(rr));
+end
+for ii = 1:length(whichReceptorsToZero)
+    rr = whichReceptorsToZero(ii);
+    fprintf('\tReceptor %d (desired/obtained): %0.2f, %0.2f\n',rr,0,theIsolatingContrast(rr));
+end
+for ii = 1:length(whichReceptorsToIgnore)
+    rr = whichReceptorsToIgnore(ii);
+    fprintf('\tReceptor %d,ignored (obtained): %0.2f\n',rr,theIsolatingContrast(rr));
+end
+
+% How close are spectra to subspace defined by basis?
+theBgNaturalApproxSpd = targetBasis*(targetBasis(projectIndices,:)\theBgDeviceSpd(projectIndices));
+theIsolatingNaturalApproxSpd = targetBasis*(targetBasis(projectIndices,:)\theIsolatingDeviceSpd(projectIndices));
+
+figure; clf; 
+subplot(1,2,1); hold on
+plot(wls,theBgDeviceSpd,'b','LineWidth',2);
+plot(wls,theBgNaturalApproxSpd,'r:','LineWidth',1);
+plot(wls(projectIndices),theBgDeviceSpd(projectIndices),'b','LineWidth',4);
+plot(wls(projectIndices),theBgNaturalApproxSpd(projectIndices),'r:','LineWidth',3);
+ylim([0 2]);
+subplot(1,2,2); hold on
+plot(wls,theIsolatingDeviceSpd,'b','LineWidth',2);
+plot(wls,theIsolatingNaturalApproxSpd,'r:','LineWidth',1);
+plot(wls(projectIndices),theIsolatingDeviceSpd(projectIndices),'b','LineWidth',4);
+plot(wls(projectIndices),theIsolatingNaturalApproxSpd(projectIndices),'r:','LineWidth',3);
+ylim([0 2]);
+
+
 
--- toolbox/receptorIsolate/ReceptorIsolateSpectral.m
+++ toolbox/receptorIsolate/ReceptorIsolateSpectral.m
@@ -1,9 +1,9 @@
-function [isolatingPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolate, ...
-    whichReceptorsToIgnore,whichReceptorsToMinimize, B_primary,backgroundPrimary,initialPrimary, ...
-    whichPrimariesToPin,primaryHeadRoom,maxPowerDiff,desiredContrasts,ambientSpd)
-% [isolatingPrimaries] = ReceptorIsolate(T_receptors,whichReceptorsToIsolate, ...
-%   whichReceptorsToIgnore,whichReceptorsToMinimize,B_primary,backgroundPrimary,initialPrimary, ...
-%   whichPrimariesToPin,primaryHeadRoom,maxPowerDiff,[desiredContrasts],[ambientSpd])
+function [modulatingPrimary,upperPrimary,lowerPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolate, ...
+    whichReceptorsToIgnore,B_primary,backgroundPrimary,initialPrimary, ...
+    primaryHeadroom,maxPowerDiff,targetBasis,projectIndices,targetLambda,desiredContrasts,ambientSpd)
+% [modulatingPrimary,upperPrimary,lowerPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolate, ...
+%   whichReceptorsToIgnore,B_primary,backgroundPrimary,initialPrimary, ...
+%   primaryHeadroom,maxPowerDiff,targetBasis,projectIndices,targetLambda,[desiredContrasts],[ambientSpd])
 %
 % Find the best isolating modulation around a given background.  This is a very general routine,
 % with inputs as follows.
@@ -13,18 +13,11 @@ function [isolatingPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolat
 % whichReceptorsToIgnore -  Index vector specifying receptors where we don't care what they do. Can be the empty matrix.
 %                           Why, you ask, might you want to do this?  Maybe if T_receptors contains the rods but you're
 %                           working at high light levels.
-% whichReceptorsToMinimize  Index vector specifying which receptors we want to minimize (i.e. not 0, but get close to it).
-%                           This is a vestigal argument.  Receptors listed here are actually ignored, rather than minimized.
-%                           An error will be thrown if you pass this as non-empty.
 % B_primary -               These calculations are device dependent.  B_primary is a set of basis vectors for the lights
 %                           that the device can produce, scaled so that the gamut is for the range [0-1] on each primary.
 % backgroundPrimary -       Background around which modulation will occur, in primary space.
-% initialPrimary -          There are cases in which we want to enforce specific values on certain primaries in the modulation spectra.
-%                           This is done here. In most cases, this primary vector is the background but not necessarily.
-% whichPrimariesToPin -     We can force some primaries not to be modulated.
-%                           Why, you ask, might you want to do this?  We can't exactly remember now, but it isn't doing
-%                           any harm to have the option.  Pass empty vector when you don't want to impose such a constraint.
-% primaryHeadRoom -         If you don't want to modulate all the way to the edge of the device gamut, pass a number in the
+% initialPrimary -          Initial guess for primary search.  Usually passed as backgroundPrimary.
+% primaryHeadroom -         If you don't want to modulate all the way to the edge of the device gamut, pass a number in the
 %                           range [0-1] here.  This constrains the primary settings to be within [0+primaryHeadRoom,1-primaryHeadRoom].
 %                           This can be useful for anticipating the fact that the device may get dimmer over time, so that the
 %                           stimulus you compute at time 0 remains in gamut after a subsequent calibration.
@@ -32,6 +25,9 @@ function [isolatingPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolat
 %                           use this for a device like a monitor, but for our OneLight device this prevents solutions that
 %                           wiggle rapdily as a function of wavelength.  Our intuition is that such wiggly spectra are not
 %                           as robust in their isolationg properties as smooth spectra.  Pass Inf to ignore.
+% targetBasis -             Keep produced spectrum close to the subspaced defined by this basis set.
+% projectIndices -          Only wavelengths corresponding to these indices count in the subspace evaluation.  Pass empty matrix to use them all.
+% targetLambda -            In trying to match subspace, weight the targetSpectrum difference by this scalar. Set to zero to ignore subspace.
 % desiredContrasts -        Vector of target contrasts for receptors that will be isolated.  This is useful, for example,
 %                           if you want to do something like produce a modulation with equal L and M cone contrasts with
 %                           opposite signs while at the same time silencing the S cones.  This vector should have the same
@@ -39,14 +35,7 @@ function [isolatingPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolat
 %                           the sum of the contrasts of the receptors in whichReceptorsToIsolate.
 % ambientSpd -              Spectral power distribution of the ambient light.  Optional.  Defaults to zero.
 %
-% Contrast held at zero for any receptor classes not in the lists.
-%
-% Notes:
-%   A) We don't pass the spectral sampling information, because this
-%   routine does not need it.  The spectral sampling is assumed to match
-%   across all spectral functions (receptor sensitivities, spectral
-%   primaries, and ambientSpd), and the smoothness constraint is enforced in
-%   wavelength sampling steps not absolute wavelength.
+% Contrast held at zero for any receptor classes not in the isolate/ignore lists.
 %
 % Known Bugs:
 %   A) It looks like the code that enforces gamut limitations in a manner that
@@ -57,28 +46,16 @@ function [isolatingPrimary] = ReceptorIsolate(T_receptors,whichReceptorsToIsolat
 %   motivation for thinking about this will only cross threshold if this
 %   error ever gets thrown.
 %
-% 4/1/12   dhb      Wrote it.
-% 11/15/12 dhb, ms  Remove upperVaryFactor arg.  Bound between 0 and 1-primaryHeadRoom.
-% 12/4/12  ms       Fixed a bug in the optimization object, added case
-%                   handling for smoothing spd or primaries.
-% 4/19/13  dhb, ms  Added lots of comments.  Change behavior when desiredContrasts not passed,
-%                   so as to maximize sum of contrasts of modulations,
-%                   rather than sum of activations.
-%          dhb      Change error function a little to avoid numerical issues.
-% 8/27/13  ll       Fix minor typo in variable name
-% 12/12/13 dhb      Clean up comments etc.
-% 3/10/16  ms, dhb  Generalize ms's fix to bounds for asymmetric
-%                   backgrounds to work when primaries are pinned.  Not actually tested.
-% 3/27/18  dhb      Added error check to make sure we don't try to use
-%                   unimplemented whichReceptorsToMinimize.
-%          dhb      Add check that background primaries respect requested
-%                   headroom.  This has to be true for anything else to make sense.
-%          dhb      Add check on returned primary range.
-%          dhb      For close cases, tolerance wasn't being checked right. Fixed I think.
+% 05/15/20   dhb      Wrote it from ReceptorIsolate
 
-% Check whether the desired contrasts were passed, and if so check
+%% Check and if necessary set default for projectIndices
+if (isempty(projectIndices))
+    projectIndices = 1:size(targetBasis,1);
+end
+
+%% Check whether the desired contrasts were passed, and if so check
 % consistency of its dimensions.
-if (nargin < 10)
+if (nargin < 11)
     desiredContrasts = [];
 end
 if ~isempty(desiredContrasts)
@@ -88,15 +65,15 @@ if ~isempty(desiredContrasts)
 end
 
 %% Default for ambientSpd
-if (nargin < 11 || isempty(ambientSpd))
+if (nargin < 12 || isempty(ambientSpd))
     ambientSpd = zeros(size(B_primary,1),1);
 end
 
 %% Initial guess for modulation
-x = initialPrimary;
+x = initialPrimary-backgroundPrimary;
 
 %% Figure out which receptors get zero modulation and set up constraint for this.
-whichReceptorsToZero = setdiff(1:size(T_receptors,1),[whichReceptorsToIsolate whichReceptorsToIgnore whichReceptorsToMinimize]);
+whichReceptorsToZero = setdiff(1:size(T_receptors,1),[whichReceptorsToIsolate whichReceptorsToIgnore]);
 backgroundReceptors = T_receptors*B_primary*backgroundPrimary;
 backgroundReceptorsZero = backgroundReceptors(whichReceptorsToZero);
 Aeq = T_receptors(whichReceptorsToZero,:)*B_primary;
@@ -111,59 +88,41 @@ beq = backgroundReceptorsZero;
 % The following piece of code may also only work just right if we're
 % not pinning primaries.
 primaryHeadRoomTolerance = 1e-7;
-if (any(backgroundPrimary < primaryHeadRoom - primaryHeadRoomTolerance))
+if (any(backgroundPrimary < primaryHeadroom - primaryHeadRoomTolerance))
     error('Cannot work if background primary is less than specified headroom');
 end
-if (any(backgroundPrimary > 1-primaryHeadRoom+primaryHeadRoomTolerance))
+if (any(backgroundPrimary > 1-primaryHeadroom+primaryHeadRoomTolerance))
     error('Cannot work if background primary is greater than 1 minus specified headroom');
 end
 for b = 1:size(backgroundPrimary, 1)
     if backgroundPrimary(b) > 0.5
-        vub(b) = 1-primaryHeadRoom;
-        vlb(b) = backgroundPrimary(b)-(1-primaryHeadRoom-backgroundPrimary(b));
+        vub(b) = 1-primaryHeadroom;
+        vlb(b) = backgroundPrimary(b)-(1-primaryHeadroom-backgroundPrimary(b));
     elseif backgroundPrimary(b) < 0.5
-        vub(b) = backgroundPrimary(b)+(backgroundPrimary(b)-primaryHeadRoom);
-        vlb(b) = primaryHeadRoom;
+        vub(b) = backgroundPrimary(b)+(backgroundPrimary(b)-primaryHeadroom);
+        vlb(b) = primaryHeadroom;
     elseif backgroundPrimary(b) == 0.5
-        vub(b) = 1-primaryHeadRoom;
-        vlb(b) = primaryHeadRoom;
+        vub(b) = 1-primaryHeadroom;
+        vlb(b) = primaryHeadroom;
     end
 end
 
-% If we are pinning some primaries, pin them by forcing their upper and
-% lower bounds to match their initial value.
-if (~isempty(whichPrimariesToPin))
-    vlb(whichPrimariesToPin) = initialPrimary(whichPrimariesToPin);
-    vub(whichPrimariesToPin) = initialPrimary(whichPrimariesToPin);
-end
+C = [eye(size(initialPrimary,1)) ; ...
+    -eye(size(initialPrimary,1)) ; ...
+    -eye(size(initialPrimary,1)) ; ...
+    eye(size(initialPrimary,1))];
+q = [ones(length(initialPrimary),1) - primaryHeadroom - backgroundPrimary ; ...
+     zeros(length(initialPrimary),1) - primaryHeadroom + backgroundPrimary ; ...
+     ones(length(initialPrimary),1) - primaryHeadroom - backgroundPrimary ; ...
+     zeros(length(initialPrimary),1) - primaryHeadroom + backgroundPrimary ];
+ 
+% C = [eye(size(initialPrimary,1)) ; ...
+%     -eye(size(initialPrimary,1)) ; ...
+%     ];
+% q = [ones(length(initialPrimary),1) - primaryHeadroom - backgroundPrimary ; ...
+%      zeros(length(initialPrimary),1) - primaryHeadroom + backgroundPrimary ; ...
+%      ];
 
-%% Construct constraint matrix.  This enforces the
-% smoothness constraint on the spectrum, ensuring that
-% the maximum difference between power at adjacent wavelengths
-% is less than passed value maxPowerDiff.
-%
-% We use two tacked matrices, C1 and C2, so that we can express
-% the desired absolute value constraint in terms of a set of
-% linear inequalities.
-vectorLength = size(B_primary, 1);
-C1 = zeros(vectorLength-1, vectorLength);
-for i = 1:vectorLength-1
-    C1(i,i) = 1;
-    C1(i,i+1) = -1;
-end
-
-C2 = zeros(vectorLength-1, vectorLength);
-for i = 1:vectorLength-1
-    C2(i,i) = -1;
-    C2(i,i+1) = 1;
-end
-
-% Stack the two constraint matrices and premultiply
-% by the primary basis.
-C = [C1 ; C2]*B_primary;
-
-% Tolerance vector, just expand passed maxPowerDiff.
-Q = ones(2*(vectorLength-1), 1)*maxPowerDiff;
 
 %% Fix numerical issues with vlb > vub that can sometimes come up.
 vlbTolerance = 1e-6;
@@ -177,21 +136,22 @@ end
 % Progressive smoothing seems to work better than providing final value all
 % at once.
 options = optimset('fmincon');
-options = optimset(options,'Diagnostics','off','Display','off','LargeScale','on','Algorithm','sqp', 'MaxFunEvals', 100000, 'TolFun', 1e-10, 'TolCon', 1e-10, 'TolX', 1e-10);
-x = fmincon(@(x) IsolateFunction(x,B_primary,backgroundPrimary,ambientSpd,T_receptors,whichReceptorsToIsolate,desiredContrasts,whichReceptorsToMinimize),x,C,Q,Aeq,beq,vlb,vub,[],options);
+options = optimset(options,'Diagnostics','off','Display','off','LargeScale','on','Algorithm','sqp', 'MaxFunEvals', 100000, 'TolFun', 1e-10, 'TolCon', 1e-10, 'TolX', 1e-5);
 
-% Extract the output arguments to be passed back.
-% This enforces a sanity check on the primaries.
-primaryTolerance = 2*vlbTolerance;
-x(x > 1 - primaryHeadRoom & x < 1 - primaryHeadRoom + primaryTolerance) = 1 - primaryHeadRoom ;
-x(x < primaryHeadRoom & x > primaryHeadRoom-primaryTolerance) = primaryHeadRoom;
-if (any(x > 1 - primaryHeadRoom))
-    error('Primary greater than 1 minus headroom');
+modulatingPrimary = fmincon(@(x) IsolateFunction(x,B_primary,backgroundPrimary,ambientSpd,T_receptors,whichReceptorsToIsolate,desiredContrasts,targetBasis,projectIndices,targetLambda), ...
+    x,C,q,Aeq,beq,[],[],[],options);
+
+%% Check gamut
+upperPrimary = backgroundPrimary + modulatingPrimary;
+lowerPrimary = backgroundPrimary - modulatingPrimary;
+primaryTolerance = 1e-6;
+if (any(upperPrimary > 1-primaryHeadroom+primaryTolerance))
+    error('upperPrimary too large');
 end
-if (any(x < primaryHeadRoom))
-    error('Primeary less than primary headroom');
+if (any(upperPrimary < 0+primaryHeadroom-primaryTolerance))
+    error('upperPrimary too small');
 end
-isolatingPrimary = x;
+
 
 end
 
@@ -199,33 +159,35 @@ end
 %
 % Optimization subfunction.  This mixes maximizing response of isolated
 % receptors with smoothness.
-function f = IsolateFunction(x,B_primary,backgroundPrimary,ambientSpd,T_receptors,whichReceptorsToIsolate,desiredContrasts,whichReceptorsToMinimize)
+function f = IsolateFunction(x,B_primary,backgroundPrimary,ambientSpd,T_receptors,whichReceptorsToIsolate,desiredContrasts,...
+    targetBasis,projectIndices,targetLambda)
 
 % Compute background including ambient
 backgroundSpd = B_primary*backgroundPrimary + ambientSpd;
+modulationSpd = B_primary*x + backgroundSpd;
+isolateContrast = ExcitationsToContrast(T_receptors(whichReceptorsToIsolate,:)*modulationSpd, ...
+    T_receptors(whichReceptorsToIsolate,:)*backgroundSpd);
 
-% Comptue contrasts for receptors we want to isolate.
-modulationSpd = B_primary*(x-backgroundPrimary);
-isolateContrasts = T_receptors(whichReceptorsToIsolate,:)*modulationSpd ./ (T_receptors(whichReceptorsToIsolate,:)*backgroundSpd);
-
-% We once used whichReceptorsToMinimize, but not currently.  This is here
-% to remind us that we could stick a term back in the error function that
-% uses it, if we ever need to.
-% minimizeContrasts = T_receptors(whichReceptorsToMinimize,:)*modulationSpd ./ (T_receptors(whichReceptorsToMinimize,:)*backgroundSpd);
-if (~isempty(whichReceptorsToMinimize))
-    error('Requesting receptor contrast minimization, but code that implements it is commented out.')
-end
-
+% Get contrast term
 if isempty(desiredContrasts)
     % Want the sum of the isolated receptor contrasts to be big. fmincon
     % minimizes, hence the negative sign.  Acheive this by minimizing
     % the difference between the isolatedContrasts and unity.  For
     % reasons not fully understood, this works better numerically than
     % simply minimizing the negative sum of squared contrasts.
-    f = sum((isolateContrasts-1).^2);
+    f1 = sqrt( sum((isolateContrast-1).^2) ) / norm(desiredContrasts);
 else
     % Minimize difference between desired and what we get
-    f = sum((isolateContrasts-desiredContrasts').^2);
+    f1 = sqrt( sum((isolateContrast-desiredContrasts').^2) ) / norm(desiredContrasts);
+end
+
+% Get basis term
+if (targetLambda > 0)
+    projectionSpd = targetBasis*(targetBasis(projectIndices,:)\modulationSpd(projectIndices));
+    f2 = sqrt(sum( (modulationSpd(projectIndices)-projectionSpd(projectIndices)).^2 ) ) / norm(modulationSpd(projectIndices));
+    f = f1 + targetLambda*f2;
+else
+    f = f1;
 end
 
 end
