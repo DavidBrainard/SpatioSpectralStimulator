a6137b392631106461e2a01f472355e6dd639b97
Semin-Oh <ohsemin1990@gmail.com>
2021-09-08 13:37:21 -0400

    Updates on the initialization for calibrating the projector

--- imageProcessing/LEDCalibration.m
+++ imageProcessing/LEDCalibration.m
@@ -3,11 +3,14 @@
 
 %% Initialize 
 clear all; close all; clc;
-tbUse('BrainardLabBase')
+tbUse('BrainardLabBase');
+tbUse('SpatioSpectralStimulator');
+
 tbUseProject('SpatioSpectralStimulator'); % Hook to the Dropbox for saving the calibration file
 
 addpath(genpath('/home/colorlab/Documents/MATLAB/toolboxes/VPixx')); % add VPixx toolbox ('Datapixx') to path (incl. all subfolders)
 
+%%
 % Connect to PR-670
 command_PR670 = 'sudo chmod a+rw /dev/ttyACM0';
 unix(command_PR670)
