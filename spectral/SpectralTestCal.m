2a2fb0b527c15297148602cfe7b0bc9660177556
David Brainard <brainard@psych.upenn.edu>
2021-10-01 14:51:20 -0400

    Tune up and insert routine for finding background.
    Happines.

--- spectral/SpectralTestCal.m
+++ spectral/SpectralTestCal.m
@@ -239,60 +239,15 @@ lowProjectWl = 400;
 highProjectWl = 700;
 projectIndices = find(wls > lowProjectWl & wls < highProjectWl);
 
-%% SEMIN - This code from here to ***** below should be a function.
-%
-% Maybe call it FindDesiredBackgroundPrimaries().
-%
-% NOTE: Eventually we will need to form the background as mixture of
-% each of the primaries.  This is just using primary 1.
-
-%% Some transformation matrices
-M_NaturalToXYZ = T_xyz*B_natural(:,1:3);
-M_XYZToNatural = inv(M_NaturalToXYZ);
-M_NaturalToLMS = T_cones*B_natural(:,1:3);
-M_LMSToNatural = inv(M_NaturalToLMS);
-
-%% Get background
-%
-% Start by defining a smooth spectrum with the chromaticity
-% we want.  This is not necessarily within the span of the
-% subprimaries we have, but serves as a starting point.
-targetBgxyY = [targetBgxy ; 1];
-targetBgXYZ = xyYToXYZ(targetBgxyY);
-desiredBgSpd = B_natural(:,1:3)*M_XYZToNatural*targetBgXYZ;
-desiredBgSpd = sum(halfOnSpd)*desiredBgSpd/sum(desiredBgSpd);
-desiredBgXYZ = T_xyz*desiredBgSpd;
-desiredBgxyY = XYZToxyY(desiredBgXYZ);
-
-%% Search for desired background
-%
-% Make a guess at reasonable primary values to start at.
-startingBgPrimaries = SpdToPrimary(subprimaryCalObjs{1},desiredBgSpd);
-startingBgSpd = PrimaryToSpd(subprimaryCalObjs{1},startingBgPrimaries);
-startingBgXYZ = T_xyz*startingBgSpd;
-startingBgxyY = XYZToxyY(startingBgXYZ);
-
-% You can play with targetLambda a bit by hand to determine tradeoff between
-% smoothness and obtaining desired chromaticity.
+%% Find background primaries to acheive desired xy at intensity scale of display
 primaryHeadRoom = 0;
-targetLambda = 5;
-[bgPrimariesIncr] = ReceptorIsolateSpectral(T_xyz,desiredBgXYZ,subprimaryCalObjs{1}.get('P_device'),startingBgPrimaries,startingBgPrimaries, ...
-    primaryHeadRoom,B_natural,projectIndices,targetLambda,subprimaryCalObjs{1}.get('P_ambient'),'EXCITATIONS',true);
-bgPrimaries = startingBgPrimaries + bgPrimariesIncr;
-bgSettings = PrimaryToSettings(subprimaryCalObjs{1},bgPrimaries);
-bgPrimariesQuantized = SettingsToPrimary(subprimaryCalObjs{1},bgSettings);
-bgSpd = PrimaryToSpd(subprimaryCalObjs{1},bgPrimaries);
-bgLMS = T_cones*bgSpd;
-bgXYZ = T_xyz*bgSpd;
-bgxyY = XYZToxyY(bgXYZ);
-figure; clf; hold on
-plot(wls,startingBgSpd,'r','LineWidth',4);
-plot(wls,bgSpd,'k','LineWidth',2);
-fprintf('Desired  background x,y = %0.3f,%0.3f\n',desiredBgxyY(1),desiredBgxyY(2));
-fprintf('Starting background x,y = %0.3f,%0.3f\n',startingBgxyY(1),startingBgxyY(2));
-fprintf('Obtained background x,y = %0.3f,%0.3f\n',bgxyY(1),bgxyY(2));
-fprintf('Mean value of background primaries: %0.2f\n',mean(bgPrimaries));
-%% **********************************
+targetLambda = 3;
+targetBgXYZ = xyYToXYZ([targetBgxy ; 1]);
+[bgPrimaries,obtainedBgSpd,obtainedBgXYZ] = FindDesiredBackgroundPrimaries(targetBgXYZ,T_xyz,subprimaryCalObjs{1}, ...
+    B_natural,projectIndices,primaryHeadRoom,targetLambda,'Scale',true,'Verbose',true);
+if (any(bgPrimaries < 0) | any(bgPrimaries > 1))
+    error('Oops - primaries should always be between 0 and 1');
+end
 
 %% SEMIN - Let's make this a function that takes in the desired 
 % primary contrasts, calibration objects etc. and produces the three
---
+++ toolbox/spectral/FindDesiredBackgroundPrimaries.m
@@ -0,0 +1,145 @@
+function [bgPrimaries,bgSpd,bgCoords] = FindDesiredBackgroundPrimaries(targetBgCoords,T,subPrimaryCalstructData, ...
+    B_natural,projectIndices,primaryHeadroom,targetLambda,options)
+% Find primaries for a desired background spectrum by searching the target background
+%
+% Syntax:
+%    [bgPrimaries,bgSpd,bgCoords] = FindDesiredBackgroundPrimaries(targetBgCoords,T,subPrimaryCalstructData, ...
+%        B_natural,projectIndices,primaryHeadroom,targetLambda)
+%
+% Description:
+%    This function searches for the desired background SPD which matches in the xy chromaticty
+%    based on the initial guess from the input variable 'targetBgxy'.
+%    The shape of the final result of the desired SPD is dependent on the variable 'targetLambda'
+%    which constraints the smootheness of the curve.
+%
+% Inputs:
+%    targetBgCoords -             The desired color coordinates of the background
+%                                 in the color space defined by passed T.
+%    T -                          Color matching functions in standard PTB format.
+%    subPrimaryCalstructData -    The calibration object that describes the
+%                                 device we're working with. It's
+%                                 wavelength support needs to match T and
+%                                 B_natural.
+%    B_natural -                  Basis functions to enforce the smoothness
+%                                 on the desired background spectrum.
+%                                 This also should be in standard PTB
+%                                 format and on the same wavelength support
+%                                 as T and the calibration.
+%    projectIndices -             Define wavelength range that will be used
+%                                 to enforce the smoothnes thorugh the
+%                                 projection onto an underlying basis set.
+%                                 These are the indices into the wavelength
+%                                 vector of the desired range.
+%    primaryHeadroom -            Set the headroom to prevent running into
+%                                 numerical error at the edges.
+%    targetLambda -               This number decides the level of smoothness of
+%                                 the final calculation result of the
+%                                 desired background spectrum. Higher value
+%                                 makes the curve more smoother, but it can
+%                                 possibly lose accuracy in matching the
+%                                 desired coordinates. You may want to play
+%                                 with targetLambda a bit by hand to
+%                                 determine tradeoff between smoothness and
+%                                 obtaining desired chromaticity.
+%
+% Outputs:
+%    bgPrimaries -                The primaries obtained.
+%    bgSpd -                      Spd corresponding to primaries.
+%    bgCoords -                   Coordinates corresponding to obtained primaries/spd.
+%
+% Optional key/value pairs:
+%    'Scale' -                    Boolean (default true). Scale target
+%                                 coordinates into range near middle of
+%                                 projector gamut.
+%    'Verbose' -                  Boolean (default true). Plots and
+%                                 printouts.
+%
+% History:
+%    09/28/21  smo  Started on it
+%    10/01/21  smo, gka, dhb     Clean and vet.
+
+%% Set parameters
+arguments
+    targetBgCoords
+    T
+    subPrimaryCalstructData
+    B_natural
+    projectIndices
+    primaryHeadroom
+    targetLambda
+    options.Scale (1,1) = true
+    options.Verbose (1,1) = true
+end
+
+%% Preparations for the calculations.
+%
+% This part prepares some matrix and variables which are needed for following calculation parts.
+%
+% Some transformation matrices.
+nCoords = size(T,1);
+nBases = size(B_natural,2);
+if (nCoords > nBases)
+    error('Must pass constraining basis whose dimensionality exceeds that of target color space');
+end
+M_NaturalToCoords = T*B_natural(:,1:nCoords);
+M_CoordsToNatural = inv(M_NaturalToCoords);
+
+% Half-on spectrum.
+nSubprimaries = subPrimaryCalstructData.get('nDevices');
+halfOnSubprimaries = 0.5*ones(nSubprimaries,1);
+halfOnSpd = PrimaryToSpd(subPrimaryCalstructData,halfOnSubprimaries); % This will be used to set the desired background Y value
+halfOnCoords = T*halfOnSpd;
+
+% Find scale of passed coordinates that brings
+% them into alignment with the half on coordinates.
+if (options.Scale)
+    halfOnTargetScaleFactor = targetBgCoords\halfOnCoords;
+    targetBgCoordsScale = halfOnTargetScaleFactor*targetBgCoords;
+end
+
+% Spectrum range.
+S = subPrimaryCalstructData.get('S');
+wls = SToWls(S); % This will be used to plot the results after getting the desired background calculation results.
+
+%% Get an initial desired background spd within the appropiate dimension subspace of the natural basis.
+desiredBgSpd = B_natural(:,1:nCoords)*M_CoordsToNatural*targetBgCoordsScale;
+desiredBgSpd = sum(halfOnSpd)*desiredBgSpd/sum(desiredBgSpd);
+desiredBgCoords = T*desiredBgSpd;
+
+%% Search for desired background.
+%
+% Make a guess at reasonable primary values to start at.
+startingBgPrimaries = SpdToPrimary(subPrimaryCalstructData,desiredBgSpd);
+startingBgSpd = PrimaryToSpd(subPrimaryCalstructData,startingBgPrimaries);
+startingBgCoords = T*startingBgSpd;
+
+% Optimize a desired background spectrum from the above initial guess.
+[bgPrimariesIncr] = ReceptorIsolateSpectral(T,desiredBgCoords,subPrimaryCalstructData.get('P_device'),startingBgPrimaries,startingBgPrimaries, ...
+    primaryHeadroom,B_natural,projectIndices,targetLambda,subPrimaryCalstructData.get('P_ambient'),'EXCITATIONS',true);
+bgPrimaries = startingBgPrimaries + bgPrimariesIncr;
+bgSettings = PrimaryToSettings(subPrimaryCalstructData,bgPrimaries);
+bgPrimariesQuantized = SettingsToPrimary(subPrimaryCalstructData,bgSettings);
+bgSpd = PrimaryToSpd(subPrimaryCalstructData,bgPrimaries);
+bgCoords = T*bgSpd;
+
+% Plot the results of the starting BG Spd and the obtained Bg spd.
+if (options.Verbose)
+    figure; clf; hold on;
+    plot(wls,startingBgSpd,'r','LineWidth',4);
+    plot(wls,bgSpd,'k','LineWidth',2);
+    legend('Starting BG','Obtained BG');
+    xlabel('Wavelength (nm)');
+    ylabel('Spectral power');
+
+    % Print out mean value of background primaries
+    fprintf('        Mean value of background primaries: %0.2f\n',mean(bgPrimaries));
+
+    % Print out how we did at getting the desired background coordinates
+    for cc = 1:nCoords
+        fprintf('        Desired   coorindate %d: %0.3g\n',cc,desiredBgCoords(cc));
+        fprintf('        Starting  coorindate %d: %0.3g\n',cc,startingBgCoords(cc));
+        fprintf('        Obtained  coorindate %d: %0.3g\n',cc,bgCoords(cc));
+    end
+end
+
+end
