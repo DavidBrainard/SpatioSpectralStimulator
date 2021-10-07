f00f61ac1016b5c0c9b86387e3264f1e3804cca4
SeminOh <ohsemin1990@gmail.com>
2021-10-07 12:10:42 -0400

    Update the function (MeasureDesiredTargetPrimaries)
    Now the function is working for a single primary and made a wrapper function in the main script.

--- spectral/SpectralTestCal.m
+++ spectral/SpectralTestCal.m
@@ -278,6 +278,15 @@ for pp = 1:nPrimaries
         T_cones,subprimaryCalObjs{pp},B_natural,projectIndices,primaryHeadroom,targetLambda,'ExtraAmbientSpd',extraAmbientSpd);
 end
 
+
+%% Measure the desired target primaries (THIS PART HAS BEEN UPDATED - SEMIN)
+% This result will be used to compute the image.
+%
+% Make a loop for measuring all primaries.
+for pp = 1:nPrimaries
+    [isolatingSpdMeasured(pp,:)] = MeasureDesiredTargetPrimaries(isolatingPrimaries(:,pp),subprimaryNInputLevels,subprimaryCalObjs{pp},pp,'projectorMode',true,'measurementOption',false);
+end
+
 %% How close are spectra to subspace defined by basis?
 theBgNaturalApproxSpd = B_natural*(B_natural(projectIndices,:)\bgSpd(projectIndices));
 isolatingNaturalApproxSpd1 = B_natural*(B_natural(projectIndices,:)\isolatingSpd1(projectIndices));
@@ -322,9 +331,6 @@ xlabel('Wavelength (nm)'); ylabel('Power (arb units)');
 title('Primary 3');
 %ylim([0 2]);
 
-%% Measure the desired target primaries (THIS PART HAS BEEN UPDATED - SEMIN)
-% This result will be used to compute the image.
-[targetSpdMeasured] = MeasureDesiredTargetPrimaries(isolatingPrimaries,subprimaryNInputLevels,subprimaryCalObjs,'projectorMode',true);
 
 %% Create lookup table that maps [-1,1] to desired LMS contrast at a very fine scale.
 %
--- toolbox/spectral/MeasureDesiredTargetPrimaries.m
+++ toolbox/spectral/MeasureDesiredTargetPrimaries.m
@@ -1,8 +1,8 @@
-function [targetSpdMeasured] = MeasureDesiredTargetPrimaries(targetPrimaries,subprimaryNInputLevels,subPrimaryCalstructData,options)
+function [targetSpdMeasured] = MeasureDesiredTargetPrimaries(targetPrimaries,subprimaryNInputLevels,subPrimaryCalstructData,targetPrimaryNum,options)
 % Measure the desired target primaries to use them for computing the contrast image.
 %
 % Syntax:
-%    [targetSpdMeasured] = MeasureDesiredTargetPrimaries(targetPrimaries,subprimaryNInputLevels,subPrimaryCalstructData,options)
+%    [targetSpdMeasured] = MeasureDesiredTargetPrimaries(targetPrimaries,subprimaryNInputLevels,subPrimaryCalstructData,targetPrimaryNum,options)
 %
 % Description:
 %    This measures the target primaries that we actually get and then use
@@ -14,6 +14,9 @@ function [targetSpdMeasured] = MeasureDesiredTargetPrimaries(targetPrimaries,sub
 %                                 subprimaries.
 %    subPrimaryCalstructData -    The calibration object that describes the
 %                                 device we're working with.
+%    targetPrimaryNum -           Target primary number. This is required
+%                                 when setting up the projector current input
+%                                 levels.
 %
 % Outputs:
 %    targetSpdMeasured -          Measured SPDs of the desired isolating
@@ -23,16 +26,26 @@ function [targetSpdMeasured] = MeasureDesiredTargetPrimaries(targetPrimaries,sub
 %    'projectorMode' -            Boolean (default true). Set the projector
 %                                 pulse mode either 'Normal' (true) or
 %                                 'Steady-on' (false).
+%    'measurementOption' -        Boolean (default true). Set if you want
+%                                 to proceed the measurements. If you set
+%                                 'true', measurement will be included, and
+%                                 'false' will skip the measurement. This
+%                                 will be useful if you run the code
+%                                 outside the lab and debugging the code.
 %
 % History:
-%    10/06/21  smo  Started on it
+%    10/06/21  smo                Started on it
+%    10/07/21  smo                Makes it working for a single primary and
+%                                 added a feature to skip the measurement part.
 
 %% Set parameters.
 arguments
     targetPrimaries
     subprimaryNInputLevels
     subPrimaryCalstructData
+    targetPrimaryNum
     options.projectorMode (1,1) = true
+    options.measurementOption (1,1) = true
 end
 
 %% Initialize (connect to both display and measurement device).
@@ -41,36 +54,39 @@ end
 % projector, setting the projector display state (normal/steady-on modes),
 % and talking to the measurement device to ready to run (PR670).
 %
-% Add VPixx toolbox 'Datapixx' to the path.
-toolboxDirectory = '/home/colorlab/Documents/MATLAB/toolboxes/VPixx'; % This is where Linux box store the file.
-addpath(genpath(toolboxDirectory));
-
-% Connect to the Vpixx projector.
-isReady = Datapixx('open');
-isReady = Datapixx('IsReady');
-
-% Set the projector mode.
-if (options.projectorMode)
-    commandNormal = 'vputil rw 0x1c8 0x0 -q quit'; % Normal mode (Default)
-    unix(commandNormal)
-    disp('Projector is set as Normal mode');
-else
-    commandSteadyOn = 'vputil rw 0x1c8 0x7 -q quit'; % Steady-on mode
-    unix(commandSteadyOn)
-    disp('Projector is set as Steady-on mode');
+% Measurement pre-preparations.
+if (options.measurementOption)
+    % Connect to the Vpixx projector.
+    isReady = Datapixx('open');
+    isReady = Datapixx('IsReady');
+    
+    % Add VPixx toolbox 'Datapixx' to the path.
+    toolboxDirectory = '/home/colorlab/Documents/MATLAB/toolboxes/VPixx'; % This is where Linux box store the file.
+    addpath(genpath(toolboxDirectory));
+    
+    % Give read-and-write permission to the spectroradiometer.
+    % This part is exclusivley when using the Linux box, and
+    % the PR670 is connected to the device either 'ttyACM0' or 'ttyACM1',
+    % so give permission to both here.
+    commandConnectToPR670_1 = 'sudo chmod a+rw /dev/ttyACM0';
+    unix(commandConnectToPR670_1)
+    commandConnectToPR670_2 = 'sudo chmod a+rw /dev/ttyACM1';
+    unix(commandConnectToPR670_2)
+    
+    % Connect to spectroradiometer PR670.
+    CMCheckInit(5); % Number 5 is allocated to PR670
+    
+    % Set the projector mode.
+    if (options.projectorMode)
+        commandNormal = 'vputil rw 0x1c8 0x0 -q quit'; % Normal mode (Default)
+        unix(commandNormal)
+        disp('Projector is set as Normal mode');
+    else
+        commandSteadyOn = 'vputil rw 0x1c8 0x7 -q quit'; % Steady-on mode
+        unix(commandSteadyOn)
+        disp('Projector is set as Steady-on mode');
+    end
 end
-
-% Give read-and-write permission to the spectroradiometer.
-% This part is exclusivley when using the Linux box, and
-% the PR670 is connected to the device either 'ttyACM0' or 'ttyACM1',
-% so give permission to both here.
-commandConnectToPR670_1 = 'sudo chmod a+rw /dev/ttyACM0';
-unix(commandConnectToPR670_1)
-commandConnectToPR670_2 = 'sudo chmod a+rw /dev/ttyACM1';
-unix(commandConnectToPR670_2)
-
-% Connect to spectroradiometer PR670.
-CMCheckInit(5); % Number 5 is allocated to PR670
 
 % Set the working range of subprimary channels (chanel 8 is not working at the moment).
 logicalToPhysical = [0:7 9:15];
@@ -81,56 +97,56 @@ S = subPrimaryCalstructData.get('S');
 % Set primary and subprimary numbers.
 nPrimaries = 3;
 nSubprimaries = subPrimaryCalstructData.get('nDevices');
+if (targetPrimaryNum > 3) && (targetPrimaryNum < 1)
+    error('Target primary number should be set within [1, 2, 3]')
+end
 
 % Target Spd.
 % This will be compared to the measurement result at the end.
-for pp = 1:nPrimaries
-    targetSpd(:,pp) = PrimaryToSpd(subPrimaryCalstructData,targetPrimaries(:,pp));
-end
+targetSpd = PrimaryToSpd(subPrimaryCalstructData,targetPrimaries);
 
 %% Set primary settings and measure it.
-% Display plain screen on DLP using PTB.
-PsychDefaultSetup(2); % PTB pre-setup
-screens = Screen('Screens');
-screenNumber = max(screens);
-white = WhiteIndex(screenNumber);
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, white);
-
-% Set projector settings and measure.
-otherPrimarySettings = 0;
-for pp = 1:nPrimaries
-    otherPrimaries = setdiff(1:nPrimaries,pp);
-    % Set target settings.
-    targetSettings = PrimaryToSettings(subPrimaryCalstructData,targetPrimaries(:,pp)) * subprimaryNInputLevels;
+if (options.measurementOption)
+    % Display plain screen on DLP using PTB.
+    PsychDefaultSetup(2); % PTB pre-setup
+    screens = Screen('Screens');
+    screenNumber = max(screens);
+    white = WhiteIndex(screenNumber);
+    [window, windowRect] = PsychImaging('OpenWindow', screenNumber, white);
+    
+    % Set projector input settings.
+    targetSettings = PrimaryToSettings(subPrimaryCalstructData,targetPrimaries) * subprimaryNInputLevels;
+    otherPrimaries = setdiff(1:nPrimaries,targetPrimaryNum);
+    otherPrimarySettings = 0;
+    
+    % Set projector current levels as the above settings.
     for ss = 1:nSubprimaries
-        % Set projector current levels.
-        Datapixx('SetPropixxHSLedCurrent', pp-1, logicalToPhysical(ss), round(targetSettings(ss))); % Target primary
+        Datapixx('SetPropixxHSLedCurrent', targetPrimaryNum, logicalToPhysical(ss), round(targetSettings(ss))); % Target primary
         Datapixx('SetPropixxHSLedCurrent', otherPrimaries(1)-1, logicalToPhysical(ss), otherPrimarySettings); % Other Primary 1
         Datapixx('SetPropixxHSLedCurrent', otherPrimaries(2)-1, logicalToPhysical(ss), otherPrimarySettings); % Other Primary 2
     end
+    
     % Measurement.
     targetSpdMeasured(:,pp) = MeasSpd(S,5,'all');
-    disp(sprintf('Measurement complete! - Primary %d',pp)); 
+    disp(sprintf('Measurement complete! - Primary %d',targetPrimaryNum));
+else
+    targetSpdMeasured = targetSpd; % Just put the same spd as desired for here.
+    disp(sprintf('Measurement has been skipped!'));
 end
 
 % Close PTB screen.
 sca;
 
 % Conversion factor.
-for pp = 1:nPrimaries
-    targetToMeasured(:,pp) = sum(targetSpd(:,pp))/sum(targetSpdMeasured(:,pp));
-end
+% This should be removed later on. Now just checking the shape of the spd.
+measuredToTarget = sum(targetSpd)/sum(targetSpdMeasured);
 
-% Plot the measurement result.
-figure; 
-for pp = 1:nPrimaries
-    subplot(nPrimaries,1,pp); hold on;
-    plot(SToWls(S),targetSpd(:,pp),'k-','LineWidth',1); % Target Spd.
-    plot(SToWls(S),targetSpdMeasured(:,pp).*targetToMeasured(pp),'r--','LineWidth',1); % Measured Spd.
-    xlabel('Wavelength (nm)');
-    ylabel('Spectral power');
-    legend('Target','Measurement');
-    title(sprintf('Primary %d',pp));
-end
-
+% Plot the results.
+figure; hold on;
+plot(SToWls(S),targetSpd,'k-','LineWidth',1); % Target Spd.
+plot(SToWls(S),targetSpdMeasured.*measuredToTarget,'r--','LineWidth',1); % Measured Spd.
+xlabel('Wavelength (nm)');
+ylabel('Spectral power');
+legend('Target','Measurement');
+title(sprintf('Primary %d',targetPrimaryNum));
 end
