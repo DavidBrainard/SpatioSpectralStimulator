d283e179f2d7c05c08e185f64c43e28705f74443
SeminOh <ohsemin1990@gmail.com>
2021-10-29 14:00:59 -0400

    Function still in progress

--- toolbox/spectral/SetProjectorSubprimarySettings.m
+++ toolbox/spectral/SetProjectorSubprimarySettings.m
@@ -1,4 +1,4 @@
-function [] = SetProjectorSubprimarySettings(testSubprimarySettings,options)
+function [] = SetProjectorSubprimarySettings(targetSubprimarySettings,options)
 % Set the projector subprimary settings for SACC project.
 %
 % Syntax: [] = SetProjectorSubprimarySettings(testSubprimarySettings)
@@ -30,11 +30,14 @@ function [] = SetProjectorSubprimarySettings(testSubprimarySettings,options)
 
 %% Set parameters.
 arguments
-    testSubprimarySettings
+    targetSubprimarySettings
     options.projectorMode (1,1) = true
     options.measurementOption (1,1) = true
     options.verbose (1,1) = true
 end
+
+%% Set parameters.
+nPrimaries = 3;
 
 %% Connect the projector and set the projector mode.
 % Add VPixx toolbox 'Datapixx' to the path.
@@ -56,9 +59,15 @@ else
     disp('Projector is set as Steady-on mode');
 end
 
+%% Set projector input settings.
+otherPrimaries = setdiff(1:nPrimaries,targetPrimaryNum);
+otherPrimarySubprimarySettings = 0;
 
-
-
-
+% Set projector current levels as the above settings.
+for ss = 1:nSubprimaries
+    Datapixx('SetPropixxHSLedCurrent', targetPrimaryNum-1, logicalToPhysical(ss), round(targetSubprimarySettings(ss)*(subprimaryNInputLevels-1)));   % Target primary
+    Datapixx('SetPropixxHSLedCurrent', otherPrimaries(1)-1, logicalToPhysical(ss), round(otherPrimarySubprimarySettings*(subprimaryNInputLevels-1))); % Other Primary 1
+    Datapixx('SetPropixxHSLedCurrent', otherPrimaries(2)-1, logicalToPhysical(ss), round(otherPrimarySubprimarySettings*(subprimaryNInputLevels-1))); % Other Primary 2
+end
 
 end
