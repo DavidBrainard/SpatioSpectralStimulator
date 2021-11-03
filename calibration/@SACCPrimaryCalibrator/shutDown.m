760f861dc2e2d4c903793322259ea2d3e7ddaa56
SeminOh <ohsemin1990@gmail.com>
2021-11-03 11:52:20 -0400

    Still working on SACC calibration code

--- calibration/@SACCPrimaryCalibrator/shutDown.m
+++ calibration/@SACCPrimaryCalibrator/shutDown.m
@@ -1,35 +1,25 @@
 % Method to shutdown the device
 function obj = shutDown(obj)
-    if (obj.options.verbosity > 9)
-        fprintf('In SACCPrimary.shutDown() method\n');
-    end
+if (obj.options.verbosity > 9)
+    fprintf('In SACCPrimary.shutDown() method\n');
+end
 
-    % [SEMIN]
-    % Here put all the subprimaries back into a fairly normal state.
-    % Normal mode, and some reasonable R, G, and B subprimary values.
-    
-    % Set the projector in a normal mode
-    CommandNormalMode = 'vputil rw 0x1c8 0x0 -q quit'; 
-    unix(CommandNormalMode)
+%% Here put all the subprimaries back into a fairly normal state.
+% Normal mode, and some reasonable R, G, and B subprimary values.
+%
+% Make a matrix for setting up the subprimary settings.
+subprimaryClosingSettings = zeros(obj.nPrimaries,n.Subprimaries); % Base matrix for subprimary settings.
+subprimaryClosingCurrent = obj.nInputLevels-1; % Current level.
+idxSubprimaryTurnOnDefault = [13 14 15 0; 1 5 6 7; 1 2 3 0]; % Default settings when turning on the projector (0 is for matching size of the matrix).
+for pp = 1:obj.nPrimaries
+    for ii = 1:idxSubprimaryTurnOnDefault
+        subprimaryClosingSettings(pp,ii) = subprimaryClosingCurrent;
+    end
+end
 
-    % Set the projector default settings when you can get when turning on 
-    CurrentDefault = obj.nInputLevels;
-    
-    % Set Primary1 
-    for i=[13,14,15] % SubPrimary
-        Datapixx('SetPropixxHSLedCurrent', 0, obj.logicalToPhysical(i), CurrentDefault); 
-    end
-    
-    % Set Primary2
-    for i=[1,5,6,7] % SubPrimary
-        Datapixx('SetPropixxHSLedCurrent', 1, obj.logicalToPhysical(i), CurrentDefault);
-    end
-    
-    % Set Primary3
-    for i=[1,2,3] % SubPrimary 
-        Datapixx('SetPropixxHSLedCurrent', 2, obj.logicalToPhysical(i), CurrentDefault); 
-    end
+% Set subprimary settings here.
+SetSubprimarySettings(subprimaryClosingSettings,'projectorMode',true);
 
-    % Close everything. Here we should also reset the PTB verbosity
-    sca;
+% Close everything. Here we should also reset the PTB verbosity
+sca;
 end
