39bde5808fcc52c0c69fd7408b35b715c235f297
Semin-Oh <ohsemin1990@gmail.com>
2021-11-24 16:23:37 -0500

    Working draft of DLP Stability check

--- spectral/screen stability/SACC_ScreenStabilityCheck.m
+++ spectral/screen stability/SACC_ScreenStabilityCheck.m
@@ -1,76 +1,121 @@
 % SACC_ScreenStabilityCheck
 %
-% This tests screen stability for SACC project. We want to know how 
+% This tests screen stability for SACC project. It basically measures
+% the plain screen repeatedely and 
 % 
 % History:
-%    11/24/2021 smo   Pulled out the part from the old code and cleaned
-%                     tried to follow our convention for variable names etc.
+%    11/24/2021 smo   Pulled out this part from the old code. It has been
+%                     cleaned up using our SACC measurement functions.
 
-%% ******************************************************ROUTINE STARTS FROM HERE****************************************************************
-%% STABILITY TEST OVER TIME
-% Measure the spectrum automatically in a specific time interval
+%% Set parameters here.
+% 
+% This code basically measures the spectrum of the plain screen
+% automatically in a specific time interval.
 
-% Set the measurement date (it reflects the saved file name)
-Date = '0723'; 
+% Set measurement time length and interval. The time is set in minute unit,
+% which will be converted into second unit later in this code.
+totalMeasurementTime_min = 0.2;
+timeDelayBeforeMeasurement_min = 0.1;
+timeDelayBeforeMeasurement_sec = timeDelayBeforeMeasurement_min * 60;
 
-% Time delay before start (go out the room before it's done)
-timesec = 15;
-t = timer('TimerFcn','stat=false; disp(''Time OFF to leave!'')','StartDelay',timesec);
-start(t)
-stat=true;
-while(stat==true)
-     disp('.')
+% Measurement range.
+S = [380 2 201];
+
+% Verbose.
+verbose = true;
+
+%% Make a bit of time delay before the measurement starts. 
+% You can go out before it's done.
+timeDelayGoOut_sec = 3;
+for tt = 1:timeDelayGoOut_sec
      pause(1) % Pause for 1 seconds (unit)
 end
-delete(t)
 
-% Connect PR670
-CMCheckInit(5) % CMCheckInit([meterType], [PortString]) / 5 is allocated to PR670
-S=[380 2 201]; % Wavelength range (380-780 nm) with 2 nm interval
+%% Make screen and spectroradiometer ready.
+%
+% Open the plain screen. Simply set it as white here. It won't change
+% during the whole measurements.
+screenSettings = [1 1 1];
+OpenPlainScreen(screenSettings,'projectorMode',true,'verbose',verbose);
 
-% Set the measurement time length in min
-time_min = 60; % time in minutes
-timedelay_min = 0.5;
+% Connect to the spectroradiometer. We will use PR670 here.
+OpenSpectroradiometer;
 
-time_sec = time_min*60;
-timedelay_sec = timedelay_min*60;
+%% Measurements.
+% 
+% The number of measurements are calculated based on the total measurement
+% time and time delay interval which were set from the above.
+%
+% +1 is the measurement at the cold state which is right after the screen turned on.
+nMeasurments = (totalMeasurementTime_min / timeDelayBeforeMeasurement_min) + 1; 
+allSpdMeasured = zeros(S(3),nMeasurments);
 
-NumMeasurements = time_min/timedelay_min + 1; % The number of measurement repetitions / +1 for the measurement at cold (right after turned on)
-
-for i=1:NumMeasurements
+% Measure it.
+for i=1:nMeasurments
     
-    % PR670 measure
-    fw(:,i)=MeasSpd(S,5,'all'); % Measurement (it takes a while)
-
-    % Time delay
-    t = timer('TimerFcn','stat=false; disp(''Measurement starts!'')','StartDelay',timedelay_sec);
-    start(t)
-    stat=true;
-    while(stat==true)
-    disp('.')
-    pause(1) % Pause for 1 seconds (unit)
+    % Measurement happens here.
+    allSpdMeasured(:,i) = MeasureSpectroradiometer;
+    
+    % Make a delay before next measurement.
+    for tt = 1:timeDelayBeforeMeasurement_sec
+        % Pause for 1 second in unit.
+        pause(1);
     end
-    delete(t)
-    
 end
 
-% Save the spectrum and XYZ values
-filename = append(Date,'Spectrum','.mat'); 
-save(filename,'fw') % Save the variable as a '.mat' file
+%% XYZ calculations
+%
+% Match the wavelength range.
+load T_xyzJuddVos;
+T_xyz = SplineCmf(S_xyzJuddVos, 683*T_xyzJuddVos, S);
 
-% Save the XYZ values
-% Wavelength linear interpolation (1/2/5 nm interval)
-w_2nm = [380:2:780]; % Given data
-w_1nm = [380:1:780]; % Interpolation range
-fw_1nm = interp1(w_2nm,fw,w_1nm);
-fw_5nm = fw_1nm(1:5:length(fw_1nm),:); % Data in 5 nm interval
+% Calculate XYZ values.
+XYZ = 683 * T_xyz * allSpdMeasured;
+xyY = XYZToxyY(XYZ);
 
-load T_xyzJuddVos % Judd-Vos XYZ Color matching function
-T_XYZ = T_xyzJuddVos';
-XYZ = 683*fw_5nm'*T_XYZ; % XYZ calculation
-% xyY = XYZToxyY(XYZ'); % xyY
-% colorgamut=XYZToxyY(T_XYZ');
-% colorgamut(:,82)=colorgamut(:,1);
+% Calculate color gamut too.
+colorGamut = XYZToxyY(T_xyz);
+colorGamut(:,end+1) = colorGamut(:,1);
 
-filename2 = append(Date,'XYZ','.mat'); 
-save(filename2,'XYZ') 
+%% Plot the data.
+if (verbose)
+   % Spds.
+   figure; clf;
+   plot(allSpdMeasured);
+   xlabel('Wavelenth (nm)');
+   ylabel('Spectral power distribution');
+
+   % Luminance.
+   figure; clf;
+   subplot(1,2,1);
+   measurementTime = linspace(0, totalMeasurementTime_min, nMeasurments);
+   plot(measurementTime, XYZ(3,:),'r*--');
+   xlabel('Measurement time (min)');
+   ylabel('Luminance (cd/m2)');
+   ylim([max(XYZ(3,:))*0.8 max(XYZ(3,:))*1.2]);
+   legend('Measurements');
+   
+   % xy coordiantes.
+   subplot(1,2,2); hold on;
+   plot(xyY(1,:), xyY(2,:), 'r*');
+   plot(colorGamut(1,:),colorGamut(2,:),'k-');
+   xlabel('CIE x');
+   ylabel('CIE y');
+   legend('Measurements','Color Gamut');
+end
+
+%% Save the data.
+%
+% Close the screen and projector.
+CloseScreen;
+CloseSpectroradiometer;
+
+% Save data with the name containing dayTimestr.
+if (ispref('SpatioSpectralStimulator','CheckDataFolder'))
+    testFiledir = getpref('SpatioSpectralStimulator','CheckDataFolder');
+    dayTimestr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
+    testFilename = fullfile(testFiledir,sprintf('additivityCheck_%s',dayTimestr));
+    save(testFilename,'allSpdMeasured','XYZ','xyY','colorGamut', ...
+                      'measurementTime','S');
+end
+ 
