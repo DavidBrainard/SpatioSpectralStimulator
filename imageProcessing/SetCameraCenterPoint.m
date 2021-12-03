9eb550458bc994f95d6e6efc2417a14d9b3d0d7a
Semin-Oh <ohsemin1990@gmail.com>
2021-12-03 16:23:27 -0500

    Remove the part searching the circles on the image

--- imageProcessing/SetCameraCenterPoint.m
+++ imageProcessing/SetCameraCenterPoint.m
@@ -5,9 +5,11 @@
 % camera should be connected. It basically uses the Image acquisition
 % toolbox, and the add-ons 'Gentl' and 'Gig' should be also installed to
 % identify the camera.
-%
+
 % History:
 %    11/30/21 smo    Cleaned up and tried to write it in the format.
+%    12/03/21 smo    Removed the finding centered point part and make it as
+%                    a separate code (see FindCircleImage.m)
 
 %% Initialzie.
 clear; close all;
@@ -37,62 +39,3 @@ markerIndex = 0.5;
 % Display real-time camera preview screen with the center point marked.
 preview(vid, hImage);
 centerPoint = line(hAxes, round([markerIndex*imWidth, markerIndex*imWidth]),round([markerIndex*imHeight, markerIndex*imHeight]),'Marker','x','MarkerSize',15,'color','r','LineWidth',1,'LineStyle','none');
-
-%% Save the image and find the centered point.
-%
-% This part captures and saves the image and finds the circle on the image
-% to decide it as the centered point. 
-FINDCENTER = false;
-if (FINDCENTER)
-    vid = videoinput('gentl', 1, 'Mono8');
-    src = getselectedsource(vid);
-    vid.FramesPerTrigger = 1; % set within 1-255
-    src.AutoExposureTimeLowerLimit = 48;
-    src.AutoExposureTimeUpperLimit = 10000;
-    src.AutoFunctionROIWidth = 2780;
-    
-    start(vid);
-    viddata = getdata(vid);
-    imwrite(viddata,'Test.tiff');
-    
-    % Load an image and find targeted circle
-    image = imread('Test.tiff','tif');
-    [Ypixel Xpixel] = size(image);
-    
-    figure(1); imshow(image); title('Original Image');
-    
-    % Find size of circles we want
-    d = imdistline; % measure the radius of the target circle image
-    
-    image_filtered = imfilter(image,ones(5)/25);
-    
-    % LCPA1 image
-    [centers, radii] = imfindcircles(image_filtered,[10 20]/2,'ObjectPolarity','dark','Sensitivity',0.92,'EdgeThreshold',0.05);
-    % [10 18] for the position 1
-    % [14 22] for the position 2
-    
-    h = viscircles(centers,radii);
-    
-    % Find targeted circles
-    figure(2); imshow(image_filtered); title('Image with targeted points'); hold on;
-    
-    % LCPA1 centered point
-    lowerlimit_X = 0.485;
-    upperlimit_X = 0.515;
-    lowerlimit_Y = 0.485;
-    upperlimit_Y = 0.515;
-    
-    index_target_X = find(centers(:,1)>Xpixel*lowerlimit_X & centers(:,1)<Xpixel*upperlimit_X); % sort of handpicked targeted points...
-    index_target_Y = find(centers(:,2)>Ypixel*lowerlimit_Y & centers(:,2)<Ypixel*upperlimit_Y);
-    
-    index_target = intersect(index_target_X,index_target_Y);
-    
-    radii_target = radii(index_target);
-    centers_target = centers(index_target,:); % [Xpixel Ypixel]
-    h_target = viscircles(centers_target,radii_target);
-    
-    position_error = [centers_target(1)/Xpixel centers_target(2)/Ypixel]
-    
-    txt = num2str(round(position_error,4));
-    text(centers_target(1),centers_target(2),txt,'HorizontalAlignment','left','color','r')
-end
