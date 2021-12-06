053bcf0d8d1093347600f189ef6cf9b2845456ca
Semin-Oh <ohsemin1990@gmail.com>
2021-12-06 12:05:34 -0500

    More cleaned up and substitute the parts with our SACC function

--- imageProcessing/SetScreenTestpattern.m
+++ imageProcessing/SetScreenTestpattern.m
@@ -5,66 +5,24 @@
 
 % History:
 %    12/6/21 smo   Cleaned up.
+
 %% Start over.
 sca;
 close all;
 clear all;
 
-%% Set basic Psychtoolbox settings.
-%
-% Here we call some default settings for setting up Psychtoolbox
-PsychDefaultSetup(2);
-screens = Screen('Screens');    
-screenNumber = max(screens);
-
-% Define black and white (white will be 1 and black 0). 
-white = WhiteIndex(screenNumber);
-black = BlackIndex(screenNumber);
-
-% Open window in either white or black.
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, white);
-
-% Get the size of the on screen window in pixels.
-[screenXpixels, screenYpixels] = Screen('WindowSize', window);
-
-% Get the centre coordinate of the window in pixels
-[xCenter, yCenter] = RectCenter(windowRect);
-
-% Enable alpha blending for anti-aliasing
-Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+%% Initialize.
+[window, windowRect] = OpenPlainScreen([1 1 1]);
+screenXpixels = windowRect(3);
+screenYpixels = windowRect(4);
 
 %% Set which type of screen to display here.
 % 
-% Set one among [Rectangle, Circle, Contrast, SingleLine]
+% Set one among [Circle, Contrast, SingleLine]
 ScreenPatternType = 'SingleLine';
 
 switch ScreenPatternType
-    case 'Rectangle'
-        
-        % Draw lines for Rectangle diagoanl elements
-        lineColor = [0 0 0];
-
-        fromH = 0; % starting point of the line (x)
-        fromV = 0; % starting point of the line (y)
-        toH = screenXpixels; % end point of the line (x)
-        toV = screenYpixels; % end point of the line (y)
-
-        fromH2 = screenXpixels; % starting point of the line (x)
-        fromV2 = 0; % starting point of the line (y)
-        toH2 = 0; % end point of the line (x)
-        toV2 = screenYpixels; % end point of the line (y)
-
-        Screen('DrawDots', window, [dotXpos dotYpos], theClut, dotSizePix, dotColor, [], 2);
-        Screen('DrawLine', window, lineColor, fromH, fromV, toH, toV,3); %Screen(‘DrawLine’, windowPtr [,color], fromH, fromV, toH, toV [,penWidth]);
-        Screen('DrawLine', window, lineColor, fromH2, fromV2, toH2, toV2,3); %Screen(‘DrawLine’, windowPtr [,color], fromH, fromV, toH, toV [,penWidth]);
-
-        Screen('DrawLine', window, lineColor, 0, 0, screenXpixels, 0,7); % edge horizontal lines
-        Screen('DrawLine', window, lineColor, 0, screenYpixels, screenXpixels, screenYpixels,7);
-
-        Screen('DrawLine', window, lineColor, screenXpixels, 0, screenXpixels,screenYpixels,7); % edge vertical lines
-        Screen('DrawLine', window, lineColor, 0, 0, 0,screenYpixels,7);
-
-    case 'Circle'
+     case 'Circle'
         [xCenter, yCenter] = RectCenter(windowRect);
         size_circle = 200;
         baseRect = [0 0 size_circle size_circle];
@@ -110,30 +68,30 @@ switch ScreenPatternType
 
     case 'SingleLine'
         [xCenter, yCenter] = RectCenter(windowRect);
-        rectColor = [0 0 0];
+        rectColor = [0 0 0];     
 
-        % Set the barwidth
+        % Set the barwidth and directions.
         rectColor = [0 0 0];
-        barWidthPixel = 5; % in pixel unit
+        barWidthPixel = 5; 
         whichSideBar = 'vertical'; 
         
-        % Single rectangle
-    switch whichSideBar
-        case 'vertical'
-        baseRect = [0 0 barWidthPixel screenYpixels];
-        centeredRect = CenterRectOnPointd(baseRect, xCenter-barWidthPixel/2, yCenter);
-        Screen('FillRect', window, rectColor, centeredRect); % Fill in black for the area
-        Screen('Flip', window);
+        switch whichSideBar
+            case 'vertical'
+            baseRect = [0 0 barWidthPixel screenYpixels];
+            centeredRect = CenterRectOnPointd(baseRect, xCenter-barWidthPixel/2, yCenter);
+            Screen('FillRect', window, rectColor, centeredRect); % Fill in black for the area
+            Screen('Flip', window);
+
+            case 'horizontal'
+            baseRect = [0 0 screenXpixels barWidthPixel];
+            centeredRect = CenterRectOnPointd(baseRect, xCenter-barWidthPixel/2, yCenter);
+            Screen('FillRect', window, rectColor, centeredRect); % Fill in black for the area
+            Screen('Flip', window);
+        end
         
-        case 'horizontal'
-        baseRect = [0 0 screenXpixels barWidthPixel];
-        centeredRect = CenterRectOnPointd(baseRect, xCenter-barWidthPixel/2, yCenter);
-        Screen('FillRect', window, rectColor, centeredRect); % Fill in black for the area
-        Screen('Flip', window);
-    end
     otherwise 
 end
 
 %% Activate the following commands if you want to quit the screen with key stroke
 KbStrokeWait;
-sca;
+CloseScreen;
