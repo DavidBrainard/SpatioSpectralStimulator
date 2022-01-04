942a342a976ce1fda0aaf5ebe67efcf44ab27b4b
SeminOh <ohsemin1990@gmail.com>
2022-01-04 11:29:39 -0500

    Working draft without using PTB

--- experiment/SACC_runExperiment.m
+++ experiment/SACC_runExperiment.m
@@ -10,11 +10,19 @@
 clear all; close all;
 
 %% Set paratmeters.
+%
+% If you are not acutally running the experiment, set TURNONSCREEN as false
+% and just image will show up instead of displaying on PTB. It will be
+% helpful to check and debug.
 initialScreenSettings = [1 1 1];
-nTestImage = 20;
+nTrials = 10;
+
+TURNONSCREEN = false;
 
 %% Open the screen.
-OpenPlainScreen(initialScreenSettings);
+if(TURNONSCREEN)
+    OpenPlainScreen(initialScreenSettings);
+end
 
 %% Running trials.
 %
@@ -22,9 +30,10 @@ OpenPlainScreen(initialScreenSettings);
 % Image is stored in 'ScreenSettingsImage'
 %
 % Maybe we want to save the test images in the same .mat file so that we
-% can load the file and read the images from the same source of the file. 
+% can load the file and read the images from the same source of the file.
 conditionName = 'LminusMSmooth';
 
+% Load test image file.
 if (ispref('SpatioSpectralStimulator','TestDataFolder'))
     testFiledir = getpref('SpatioSpectralStimulator','TestDataFolder');
     testFilename = fullfile(testFiledir,sprintf('testImageData_%s',conditionName));
@@ -32,44 +41,47 @@ if (ispref('SpatioSpectralStimulator','TestDataFolder'))
 end
 image = theData.screenSettingsImage;
 
-% Display the test image.
+% Display test image.
 %
 % Now it just displays an image, but this part will be substituted with a
 % separate function displaying the image using PTB later on.
-figure; clf;
-imshow(image);
-disp('Test image is displaying!');
-
-% Get a response either Yes or No.
-disp('Waiting for the key is pressed');
-
-% One way to do it
-%
-% 28 leftarrow
-% 29 rightarrow
-% 30 uparrow
-% 31 downarrow
-gettingResponse = waitforbuttonpress;
-response = double(get(gcf,'CurrentCharacter'));
-close;
-
-% Another way to do it.
-[~,~,response2] = ginput(1);
-switch response2
-    case 28
-    case 29
+for tt = 1:nTrials
+    figure; clf;
+    imshow(image);
+    title(append('Trial ',num2str(tt)),'fontsize',15);
+    fprintf('Test image 1 - trial %d is displaying and waiting for the key is pressed... \n',tt);
+    
+    % Get a response either Yes or No.
+    %
+    % It can be also done by using 'ginput'. But, here we used the function
+    % waitforbuttonpresss.
+    %
+    % Following is the ASCII allocated number for the keyboards.
+    % 28 leftarrow
+    % 29 rightarrow
+    % 30 uparrow
+    % 31 downarrow
+    gettingResponse = waitforbuttonpress;
+    response(tt) = double(get(gcf,'CurrentCharacter'));
+    close;
+    fprintf('     Key input has been received! \n');
 end
-close;
+
+fprintf('Test image 1 evalaution complete! \n');
 
 %% Close.
-CloseScreen;
+if(TURNONSCREEN)
+    CloseScreen;
+end
 
 % Save the response data.
-% 
+%
 % We may want to change the folder later on. But, now it is set to the same
 % folder where the test image is stored.
-if (ispref('SpatioSpectralStimulator','TestDataFolder'))
-    testFiledir = getpref('SpatioSpectralStimulator','TestDataFolder');
-    testFilename = fullfile(testFiledir,sprintf('expResponseData_%s',conditionName));
-    save(testFilename,'response');
+if(TURNONSCREEN)
+    if (ispref('SpatioSpectralStimulator','TestDataFolder'))
+        testFiledir = getpref('SpatioSpectralStimulator','TestDataFolder');
+        testFilename = fullfile(testFiledir,sprintf('expResponseData_%s',conditionName));
+        save(testFilename,'response');
+    end
 end
