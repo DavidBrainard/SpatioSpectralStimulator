4e2762f9630587f2a73dc638696638ba84ff24d2
SeminOh <ohsemin1990@gmail.com>
2022-03-30 10:10:09 -0400

    (SEMIN) Updates on the check and measure spectrum/power codes

--- testingChannels/CheckChannelSpd.m
+++ testingChannels/CheckChannelSpd.m
@@ -1,6 +1,9 @@
 % CheckChannelSpd
 %
 % This is to check channel power measuremtns.
+%
+% See Also:
+%    MeasureChannelSpd.
 
 % History:
 %    12/01/21 smo       - Started on it for checking the spds of the new projector
@@ -19,121 +22,131 @@ nChannels = 16;
 
 targetChannels = [2 4 6 8 10 12];
 nTargetChannels = length(targetChannels);
-targetChannelSettingValue = 1;
-logicalToPhysical = [0:15];
-
-screenSettings = [1 1 1]';
 
 % Power meter settings.
 powerMeterWl = 550;
 wls = SToWls(S);
 powerMeterWlIndex = find(wls == powerMeterWl);
 
-projectorModeNormal = true;
+projectorModeNormal = false;
 VERBOSE = true;
 
+%% Load spectrum data here.
+DEVICE = 'PR670';
 
-
-%% Load the data.
-LOADDATA = true;
-if (LOADDATA)
-    %% Load spectrum data here.
-    DEVICE = 'PR670';
-    
-    % Make a string for save file name.
-    switch projectorModeNormal
-        case true
-            projectorMode = 'normal';
-        case false
-            projectorMode = 'steady-on';
-    end
-    
-    % Load the data here.
-    if (ispref('SpatioSpectralStimulator','CheckDataFolder'))
-        testFiledir = getpref('SpatioSpectralStimulator','CheckDataFolder');
-        testFilename = GetMostRecentFileName(testFiledir,sprintf('SpdData_%s_%s',DEVICE,projectorMode));
-        prData = load(testFilename);
-    else
-        error('Cannot find data file');
-    end
-    
-    for pp = 1:nPrimaries
-        targetChPeakWls(:,pp) = FindPeakSpds(prData.spdMeasured{pp},'verbose',false);
-    end
-    
-    %% Load powermeter data here.
-    curDir = pwd;
-    cd(testFiledir);
-    
-    % Data with fixed wavelength sensitivity (550 nm).
-    powerSingleNormalWatt = xlsread('PowerMeterProcessedData.xlsx','NormalSingle');
-    powerSingleSteadyOnWatt = xlsread('PowerMeterProcessedData.xlsx','SteadyOnSingle');
-    powerWhiteNormalWatt = xlsread('PowerMeterProcessedData.xlsx','NormalWhite');
-    powerWhiteSteadyOnWatt = xlsread('PowerMeterProcessedData.xlsx','SteadyOnWhite');
-
-    % Data with different wavelength.
-    % Make a string for save file name.
-    switch projectorModeNormal
-        case true
-            projectorMode = 'NormalMode';
-        case false
-            projectorMode = 'SteadyOn';
-    end
-    
-    DEVICE = 'PowerMeter';
-    date = '0329';
-    fileType = '.csv';
-    
-    targetChPeakWls = targetChPeakWls(:,1);
-    dataRange = 'D17:D17';
-    
-    % Load sinlge peak data here.
-    for pp = 1:nPrimaries
-        for cc = 1:nTargetChannels
-            targetChPeakWl = targetChPeakWls(cc);
-            targetCh = targetChannels(cc);
-            fileName = append(DEVICE,'_',projectorMode,'_Primary',...
-                num2str(pp),'_Ch',num2str(targetCh),'_',num2str(targetChPeakWl),'nm_',date,fileType);
-            readFile = readmatrix(fileName, 'Range', dataRange);
-            powerMeterWatt(cc,pp) = readFile;
-        end
-    end
-    
-    % Load white data here.
-    for cc = 1:nTargetChannels
-        targetChPeakWl = targetChPeakWls(cc);
-        fileName = append(DEVICE,'_',projectorMode,'_White_',num2str(targetChPeakWl),'nm_',date,fileType);
-        readFile = readmatrix(fileName, 'Range', dataRange);
-        powerMeterWhiteWatt(cc,:) = readFile;
-    end
-    
-    %% Load power meter spectral sensitivity here.
-    powerMeterSensitivity = xlsread('PowerMeterResponsivityLocal.xlsx');
-    T_powerMeter = SplineCmf(powerMeterSensitivity(:,1),powerMeterSensitivity(:,2)',S);
-
-    % Normalize power meter sensitivity so that wl set during measurement
-    % is one.
-    T_powerMeter = T_powerMeter/T_powerMeter(powerMeterWlIndex);
-    
-    % Plot it.
-    figure; clf; hold on;
-    plot(powerMeterWl,T_powerMeter(powerMeterWlIndex),...
-        'o','MarkerFaceColor','r','MarkerEdgeColor',zeros(3,1),'MarkerSize',7);
-    plot(wls,T_powerMeter','k');
-    xlabel('Wavelength (nm)','FontSize',15');
-    ylabel('Normalized sensitivity', 'FontSize', 15);
-    legend('Normalized criteria');
-    
-    % Back to the current path.
-    cd(curDir);
+% Make a string for save file name.
+% It used to be normal / steady-on
+switch projectorModeNormal
+    case true
+        projectorMode = 'Normal';
+    case false
+        projectorMode = 'steady-on';
 end
 
-% Power comes in as watts! Convert to mW.
+% Load the data here.
+if (ispref('SpatioSpectralStimulator','CheckDataFolder'))
+    testFiledir = getpref('SpatioSpectralStimulator','CheckDataFolder');
+    testFilename = GetMostRecentFileName(testFiledir,sprintf('SpdData_%s_%s',DEVICE,projectorMode));
+    prData = load(testFilename);
+else
+    error('Cannot find data file');
+end
+
+for pp = 1:nPrimaries
+    targetChPeakWls(:,pp) = FindPeakSpds(prData.spdMeasured{pp},'verbose',false);
+end
+
+%% Load powermeter data here.
+curDir = pwd;
+cd(testFiledir);
+
+% DATASET1
+% Data with fixed wavelength sensitivity (550 nm).
+powerSingleNormalWatt = xlsread('PowerMeterProcessedData.xlsx','NormalSingle');
+powerSingleSteadyOnWatt = xlsread('PowerMeterProcessedData.xlsx','SteadyOnSingle');
+powerWhiteNormalWatt = xlsread('PowerMeterProcessedData.xlsx','NormalWhite');
+powerWhiteSteadyOnWatt = xlsread('PowerMeterProcessedData.xlsx','SteadyOnWhite');
+
+% DATASET2
+% Data with different wavelength.
+% Make a string for save file name.
+switch projectorModeNormal
+    case true
+        projectorMode = 'NormalMode';
+    case false
+        projectorMode = 'SteadyOnMode';
+end
+
+DEVICE = 'PowerMeter';
+date = '0329';
+fileType = '.csv';
+targetChPeakWls = targetChPeakWls(:,3);
+dataRange = 'D17:D17';
+
+% Load sinlge peak data here.
+for pp = 1:nPrimaries
+    for cc = 1:nTargetChannels
+        targetChPeakWl = targetChPeakWls(cc);
+        targetCh = targetChannels(cc);
+        fileName = append(DEVICE,'_',projectorMode,'_Primary',...
+            num2str(pp),'_Ch',num2str(targetCh),'_',num2str(targetChPeakWl),'nm_',date,fileType);
+        readFile = readmatrix(fileName, 'Range', dataRange);
+        powerMeterWatt(cc,pp) = readFile;
+    end
+end
+
+% Load white data here.
+for cc = 1:nTargetChannels
+    targetChPeakWl = targetChPeakWls(cc);
+    fileName = append(DEVICE,'_',projectorMode,'_White_',num2str(targetChPeakWl),'nm_',date,fileType);
+    readFile = readmatrix(fileName, 'Range', dataRange);
+    powerMeterWhiteWatt(cc,:) = readFile;
+end
+
+%% Load power meter spectral sensitivity here.
+powerMeterSensitivity = xlsread('PowerMeterResponsivityLocal.xlsx');
+T_powerMeterRaw = SplineCmf(powerMeterSensitivity(:,1),powerMeterSensitivity(:,2)',S);
+
+% Normalize power meter sensitivity so that wl set during measurement
+% is one.
+T_powerMeterMatch = T_powerMeterRaw/T_powerMeterRaw(powerMeterWlIndex);
+
+% Plot it.
+figure; clf; hold on;
+plot(powerMeterWl,T_powerMeterMatch(powerMeterWlIndex),...
+    'o','MarkerFaceColor','r','MarkerEdgeColor',zeros(3,1),'MarkerSize',7);
+plot(wls,T_powerMeterMatch','k');
+xlabel('Wavelength (nm)','FontSize',15');
+ylabel('Normalized sensitivity', 'FontSize', 15);
+legend('Normalized criteria');
+
+% Back to the current path.
+cd(curDir);
+
+%% Power comes in as watts! Convert to mW.
 wattToMWatt = 1000;
+
+% DATASET1
 powerSingleNormalMW = powerSingleNormalWatt .* wattToMWatt;
 powerSingleSteadyOnMW = powerSingleSteadyOnWatt .* wattToMWatt;
 powerWhiteNormalMW = powerWhiteNormalWatt .* wattToMWatt;
 powerWhiteSteadyOnMW = powerWhiteSteadyOnWatt .* wattToMWatt;
+
+% DATASET2
+powerMeterMW = powerMeterWatt .* wattToMWatt;
+powerMeterWhiteMW = powerMeterWhiteWatt .* wattToMWatt;
+
+% Resize power meter data.
+if (projectorModeNormal)
+    powerMeterSingle = powerSingleNormalMW;
+    powerMeterWhite = powerWhiteNormalMW;
+else
+    powerMeterSingle = powerSingleSteadyOnMW;
+    powerMeterWhite = powerWhiteSteadyOnMW;
+end
+
+% Single peak data. Matching the shpae with the spectrum data.
+powerMeterSingle = reshape(powerMeterSingle,length(targetChannels),nPrimaries);
 
 %% Plot measured spectra.
 if (VERBOSE)
@@ -154,53 +167,27 @@ if (VERBOSE)
     ylabel('Spectral Irradiance','FontSize',15);
 end
 
-%% Plot power meter data.
-if (VERBOSE)
-    % Get the projector mode info.
-    if (projectorModeNormal)
-        powerMeterSingle = powerSingleNormalMW;
-        powerMeterWhite = powerWhiteNormalMW;
-    else
-        powerMeterSingle = powerSingleSteadyOnMW;
-        powerMeterWhite = powerWhiteSteadyOnMW;
-    end
-    
-    % Single peak data. Matching the shpae with the spectrum data.
-    powerMeterSingle = reshape(powerMeterSingle,length(targetChannels),nPrimaries);
-    
-    % Plot here.
-    for pp = 1:nPrimaries
-        figure; clf;
-        plot(powerMeterSingle(:,pp),'ro')
-        xlabel('Target channel','FontSize',15);
-        ylabel('Power meter (mw)','FontSize',15);
-        title(append('Screen Primary: ',num2str(pp),' ',projectorMode),'FontSize',15);
-    end
-    
-    % White data.
-    figure; clf;
-    plot(powerMeterWhite,'bo');
-    xlabel('Target channel','FontSize',15);
-    ylabel('Power meter (mw)','FontSize',15);
-    title(append('White ',projectorMode),'FontSize',15);
-end
-
 %% Find scale factors for each measurement
 for pp = 1:nPrimaries
     % Grab single primary spds
     spdSingle = prData.spdMeasured{pp};
     
     % Integrate against power meter sensitivity.
-    F(:,pp) = (T_powerMeter*spdSingle)';
+    F(:,pp) = (T_powerMeterMatch * spdSingle)';
 end
 
-% Compute factors k.  
+FWhite = (T_powerMeterMatch * prData.spdMeasuredWhite);
+
+% Compute factors k.
 % These should be the same for all measurements.
-K = powerMeterSingle./F;
+k = powerMeterMW./F;
+kWhite = powerMeterWhite/FWhite;
 
 % Plot it.
-figure; clf; 
-plot(K,'ro','MarkerSize',12,'MarkerFaceColor','r');
-xlabel('Target Channels');
-ylabel('Coefficient K');
-ylim([0 1.2*max(K(:))]);
+figure; clf; hold on;
+plot(kWhite,'bo','MarkerSize',12,'MarkerFaceColor','b');
+plot(k,'ro','MarkerSize',12,'MarkerFaceColor','r');
+xlabel('Target Channels','FontSize',15);
+ylabel('Coefficient k','FontSize',15);
+ylim([0 1.2*max(k(:))]);
+legend('White','Single peak','FontSize',13);
--- testingChannels/MeasureChannelSpd.m
+++ testingChannels/MeasureChannelSpd.m
@@ -2,12 +2,17 @@
 %
 % This is to measure channel spd using either spectroradiometer or power
 % meter.
+%
+% See Also:
+%    CheckChannelSpd.
 
 % History:
-%    12/01/21 smo  Started on it for checking the spds of the new projector
-%                  for SACC project.
-%    03/28/22 smo  Added an option to select the measurement device either
-%                  PR670 or power meter.
+%    12/01/21 smo  - Started on it for checking the spds of the new projector
+%                    for SACC project.
+%    03/28/22 smo  - Added an option to select the measurement device either
+%                    PR670 or power meter.
+%    03/30/22 smo  - Deleted the option loading the data and added black
+%                    correction for spectrum measurement.
 
 %% Initialize.
 clear; close all;
@@ -16,121 +21,104 @@ clear; close all;
 S = [380 2 201];
 nPrimaries = 3;
 nChannels = 16;
+screenSettings = [1 1 1]';
 
+% Channel settings.
 targetChannels = [2 4 6 8 10 12];
 targetChannelSettingValue = 1;
+arbitraryBlack = 0.05;
 logicalToPhysical = [0:15];
-
-screenSettings = [1 1 1]';
 
 % Choose which device to use within [PR670, powermeter].
 DEVICE = 'powermeter';
 projectorModeNormal = false;
-powerMeterWaitTimeSec = 0;
+powerMeterWaitTimeSec = 5;
 VERBOSE = true;
 
 % Make a string for save file name.
 switch projectorModeNormal
     case true
-        projectorMode = 'normal';
+        projectorMode = 'Normal';
     case false
-        projectorMode = 'steady-on';
-end
-
-LOADDATA = false;
-
-%% Load the data if you want.
-if (LOADDATA)
-    if (ispref('SpatioSpectralStimulator','CheckDataFolder'))
-        testFiledir = getpref('SpatioSpectralStimulator','CheckDataFolder');
-        testFilename = GetMostRecentFileName(testFiledir,sprintf('SpdData_%s_%s',DEVICE,projectorMode));
-        load(testFilename);
-    else
-        error('Cannot find data file');
-    end
+        projectorMode = 'SteadyOn';
 end
 
 %% Open the projector screen and measurement device ready.
-if (~LOADDATA)
-    [window, windowRect] = OpenPlainScreen(screenSettings,...
-        'projectorMode',projectorModeNormal);
-    
-    if (strcmp(DEVICE,'PR670'))
-        OpenSpectroradiometer;
-    end
-    
-    %% This part measures full white.
-    channelSettingsWhite = ones(nChannels,nPrimaries);
-    SetChannelSettings(channelSettingsWhite);
+[window, windowRect] = OpenPlainScreen(screenSettings,...
+    'projectorMode',projectorModeNormal);
+
+if (strcmp(DEVICE,'PR670'))
+    OpenSpectroradiometer;
+end
+
+%% This part measures full white.
+channelSettingsWhite = ones(nChannels,nPrimaries);
+SetChannelSettings(channelSettingsWhite);
+GetChannelSettings;
+
+% Measure it.
+switch DEVICE
+    case 'PR670'
+        spdMeasuredWhite = MeasureSpectroradiometer;
+    case 'powermeter'
+        WaitSecs(powerMeterWaitTimeSec);
+    otherwise
+end
+disp('White measurement has been complete!');qq
+
+%% This part measures arbitrary black.
+if (~arbitraryBlack == 0)
+    channelSettingsBlack = ones(nChannels,nPrimaries) .* arbitraryBlack;
+    SetChannelSettings(channelSettingsBlack);
     GetChannelSettings;
     
     % Measure it.
     switch DEVICE
         case 'PR670'
-            spdMeasuredWhite = MeasureSpectroradiometer;
+            spdMeasuredBlack = MeasureSpectroradiometer;
         case 'powermeter'
             WaitSecs(powerMeterWaitTimeSec);
         otherwise
     end
-    disp('White measurement has been complete!');
-    
-    %% This part measure singe channels.
-    %
-    % Set channel settings and measure here. All data will be stored in
-    % spdmeasured.
-    nMeasureChannels = length(targetChannels);
-    
-    for pp = 1:nPrimaries
-        for cc = 1:nMeasureChannels
-            % Set channel settings here.
-            targetChannel = targetChannels(cc);
-            channelSettings = zeros(nChannels,nPrimaries);
-            channelSettings(targetChannel, pp) = targetChannelSettingValue;
-            SetChannelSettings(channelSettings);
-            
-            % Verbose.
-            fprintf('Screen primary (%d) / channel (%d) is now displaying \n', pp, targetChannel);
-            
-            % Check the current channel settings.
-            GetChannelSettings;
-            
-            % Measure it.
-            switch DEVICE
-                case 'PR670'
-                    spdChannelSingles(:,cc) = MeasureSpectroradiometer;
-                case 'powermeter'
-                    WaitSecs(powerMeterWaitTimeSec);
-                otherwise
-            end
-        end
-        if(strcmp(DEVICE,'PR670'))
-            spdMeasured{pp} = spdChannelSingles;
-        end
-    end
-    
-    %% Close.
-    if (strcmp(DEVICE,'powermeter'))
-        CloseScreen;
-    end
-    
-    if (strcmp(DEVICE,'PR670'))
-        CloseSpectroradiometer;
-    end
-    
-    %% Save the data.
-    if (strcmp(DEVICE,'PR670'))
-        % Save the file here.
-        if (ispref('SpatioSpectralStimulator','CheckDataFolder'))
-            testFiledir = getpref('SpatioSpectralStimulator','CheckDataFolder');
-            dayTimestr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
-            testFilename = fullfile(testFiledir,sprintf('SpdData_%s_%s_%s',DEVICE,projectorMode,dayTimestr));
-            save(testFilename,'S','spdMeasured','spdMeasuredWhite','targetChannels','targetChannelSettingValue');
-            disp('Data has been saved successfully!');
+    disp('Black measurement has been complete!');
+else
+    % If the black level is not set, just pass the zero spectrum.
+    spdMeasuredBlack = zeros(S(3),1);
+end
+
+%% This part measure singe channels.
+%
+% Set channel settings and measure here. All data will be stored in
+% spdmeasured.
+nMeasureChannels = length(targetChannels);
+
+for pp = 1:nPrimaries
+    for cc = 1:nMeasureChannels
+        % Set channel settings here.
+        targetChannel = targetChannels(cc);
+        channelSettings = ones(nChannels,nPrimaries) .* arbitraryBlack;
+        channelSettings(targetChannel, pp) = targetChannelSettingValue;
+        SetChannelSettings(channelSettings);
+        
+        % Verbose.
+        fprintf('Screen primary (%d) / channel (%d) is now displaying \n', pp, targetChannel);
+        
+        % Check the current channel settings.
+        GetChannelSettings;
+        
+        % Measure it.
+        switch DEVICE
+            case 'PR670'
+                spdChannelSingles(:,cc) = MeasureSpectroradiometer;
+            case 'powermeter'
+                WaitSecs(powerMeterWaitTimeSec);
+            otherwise
         end
     end
     
-    %% Say goodbye.
-    disp('All measurement has been complete!');
+    if(strcmp(DEVICE,'PR670'))
+        spdMeasured{pp} = spdChannelSingles - spdMeasuredBlack;
+    end
 end
 
 %% Find peak spectrum.
@@ -139,6 +127,30 @@ if (strcmp(DEVICE,'PR670'))
         peakSpd(:,pp) = FindPeakSpds(spdMeasured{pp},'verbose',false);
     end
 end
+
+%% Close.
+if (strcmp(DEVICE,'powermeter'))
+    CloseScreen;
+end
+
+if (strcmp(DEVICE,'PR670'))
+    CloseSpectroradiometer;
+end
+
+%% Save the data.
+if (strcmp(DEVICE,'PR670'))
+    % Save the file here.
+    if (ispref('SpatioSpectralStimulator','CheckDataFolder'))
+        testFiledir = getpref('SpatioSpectralStimulator','CheckDataFolder');
+        dayTimestr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
+        testFilename = fullfile(testFiledir,sprintf('SpdData_%s_%s_%s',DEVICE,projectorMode,dayTimestr));
+        save(testFilename,'S','spdMeasured','spdMeasuredWhite','spdMeasuredBlack','targetChannels','targetChannelSettingValue','peakSpd');
+        disp('Data has been saved successfully!');
+    end
+end
+
+%% Say goodbye.
+disp('All measurement has been complete!');
 
 %% Plot it.
 if (strcmp(DEVICE,'PR670'))
