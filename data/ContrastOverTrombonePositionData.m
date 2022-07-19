08c16cc5389691b09c06faac42ea49dcf7ff54a9
Semin-Oh <ohsemin1990@gmail.com>
2022-07-19 11:33:02 -0400

    (SEMIN) Getting ready to measure contrast over mirror positions using different lenses

--- data/ContrastOverTrombonePositionData.m
+++ data/ContrastOverTrombonePositionData.m
@@ -5,14 +5,53 @@
 
 % History:
 %    03/25/22  dhb, smo     - Measured the data and plotted the graph.
+%    07/19/22  dhb, smo     - Added data using different lenses.
 
 %% Measurement data.
 %
 % Position is in mm unit in the Trombone controlling software, and the
 % contrast was measured based on the real-time camera image.
-positionTrombone = [2.799 2.299 1.772 1.522 1.272 1.022 0.772 0.522 0.272 0.022 -0.530 -1.449 -2.193 -2.665 ];
-contrast         = [0.931 0.946 0.954 0.954 0.954 0.954 0.955 0.954 0.954 0.954  0.954  0.954  0.947  0.939 ];
+whichData = 0;
 
+switch whichData
+    case 0
+        positionTrombone = [2.799 2.299 1.772 1.522 1.272 1.022 0.772 0.522 0.272 0.022 -0.530 -1.449 -2.193 -2.665 ];
+        contrast         = [0.931 0.946 0.954 0.954 0.954 0.954 0.955 0.954 0.954 0.954  0.954  0.954  0.947  0.939 ];
+        % Lense
+    case 1
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 2
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 3
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 4
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 5
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 6
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 7
+        positionTrombone = [];
+        contrast         = [];
+        % Lense
+    case 8
+        positionTrombone = [];
+        contrast         = [];
+    otherwise
+end
+        
 contrastMax = max(contrast);
 positionContrastMax = positionTrombone(find(contrast(:) == contrastMax));
 
