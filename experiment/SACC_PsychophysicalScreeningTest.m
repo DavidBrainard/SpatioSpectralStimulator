ab8d068a3da78fb1d7b4084bf4d4a8ba76eb39d6
SeminOh <ohsemin1990@gmail.com>
2022-10-03 10:52:56 -0400

    (SEMIN) Now it loads data and test proficiency

--- experiment/SACC_PsychophysicalScreeningTest.m
+++ experiment/SACC_PsychophysicalScreeningTest.m
@@ -9,13 +9,47 @@
 %% Initialize.
 clear; close all;
 
+%% Get the current dir.
+dir = cd;
+
 %% Load the data.
 %
-% For now, we use random sampled date here.
-data = [0.6572933 0.01733973 0.2823567 0.3686444 0.3667386 0.6939998];
+% Set the dir.
+userName = 'Semin Oh';
+expName = 'Exp_0001J';
+subjectName = 'DH';
+fileDir = sprintf('~/Aguirre-Brainard Lab Dropbox/%s/MTRP_data/%s/Subject_Test_%s',userName,expName,subjectName);
+cd(fileDir);
 
-%% Calculate the interquartile range.
-medianData = median(data);
-ICR = prctile(data,75) - prctile(data,25);
+% Load the file.
+fileName1 = sprintf('TEST_%s_1',subjectName);
+fileName2 = sprintf('TEST_%s_2',subjectName);
+theData1 = readtable(fileName1);
+theData2 = readtable(fileName2);
 
-%% Print out the results.
+% Export the threshold data.
+numVarThreshold = 3;
+thresholds1 = theData1{:,numVarThreshold};
+thresholds2 = theData2{:,numVarThreshold};
+thresholds = [thresholds1; thresholds2];
+
+%% Test proficiency here.
+%
+% Subjects will be excluded for a median threshold of > 1.5% or an
+% interquartile range across the 12 measures of > 2.0%. 
+% Record as pass/fail.
+criteriaMediaThreshold = 1.5;
+criteriaICR = 2;
+
+medianThreshold = median(thresholds);
+ICR = prctile(thresholds,75) - prctile(thresholds,25);
+
+% Print out the results.
+if or(medianThreshold > criteriaMediaThreshold, ICR > criteriaICR)
+    fprintf('    FAIL! Median threshold = %.3f / ICR = %.3f \n', medianThreshold, ICR);
+else
+    fprintf('    PASS! Median threshold = %.3f / ICR = %.3f \n', medianThreshold, ICR);
+end
+
+%% Back to current dir.
+cd(dir);
