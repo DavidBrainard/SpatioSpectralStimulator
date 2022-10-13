4b1850b5aee61d55c6b2ce7ca341f9ebce99af4c
SeminOh <ohsemin1990@gmail.com>
2022-10-13 14:22:57 -0400

    (SEMIN) Now it's in a good shape

--- calibration/SACC_analyzeMonitor.m
+++ calibration/SACC_analyzeMonitor.m
@@ -2,52 +2,68 @@
 %
 % This is for quick check of the calibration results.
 
+% History:
+%    10/13/22   smo    - Wrote it.
+
 %% Initialize.
 clear; close all;
 
 %% Set dir.
+%
+% Save the current dir. We will set it back to current dir after.
 dir = cd;
 
+% Get the dir that stors the measurement files.
 fileDir = '/Users/seminoh/Aguirre-Brainard Lab Dropbox/Semin Oh/SACC_materials/Calibration';
 cd(fileDir);
 
-%% Load data.
+%% Load calibration data.
 dataSACC = load('SACC.mat').cals;
 dataSACCPrimary1 = load('SACCPrimary1.mat').cals;
 dataSACCPrimary2 = load('SACCPrimary2.mat').cals;
 dataSACCPrimary3 = load('SACCPrimary3.mat').cals;
 
-% Get the most recent data.
-nPrimaries = 3;
+% Get a number for each data which contains the most recent data.
+%
+% SACC.
 numMostRecentDataSACC = length(dataSACC);
+
+% SACC primaries.
+nPrimaries = 3;
 for pp = 1:nPrimaries
     numMostRecentDataSACCPrimary(pp) = length(eval(append('dataSACCPrimary',num2str(pp))));
 end
 
+% Extract spd and the date of measurement from each dataset. You can choose
+% multiple from each dataset if you want. It reads the data from the most
+% recent one, so if you set nData = 2, it will read the most recent one and
+% the second to the most recent.
 nData = 2;
 for dd = 1:nData
-    numTargetSACC(dd) = numMostRecentDataSACC - dd + 1;
-    numTargetSACCPrimary(:,dd) = numMostRecentDataSACCPrimary - dd + 1;
+    numTargetDataSACC(dd) = numMostRecentDataSACC - dd + 1;
+    numTargetDataSACCPrimary(:,dd) = numMostRecentDataSACCPrimary - dd + 1;
     
     % Spd.
-    spdSACC{dd} = dataSACC{numTargetSACC(dd)}.processedData.P_device;
-    spdPrimary1{dd} = dataSACCPrimary1{numTargetSACCPrimary(1,dd)}.processedData.P_device;
-    spdPrimary2{dd} = dataSACCPrimary2{numTargetSACCPrimary(2,dd)}.processedData.P_device;
-    spdPrimary3{dd} = dataSACCPrimary3{numTargetSACCPrimary(3,dd)}.processedData.P_device;
+    spdSACC{dd} = dataSACC{numTargetDataSACC(dd)}.processedData.P_device;
+    spdPrimary1{dd} = dataSACCPrimary1{numTargetDataSACCPrimary(1,dd)}.processedData.P_device;
+    spdPrimary2{dd} = dataSACCPrimary2{numTargetDataSACCPrimary(2,dd)}.processedData.P_device;
+    spdPrimary3{dd} = dataSACCPrimary3{numTargetDataSACCPrimary(3,dd)}.processedData.P_device;
     
     % Date of measurement.
-    dateSACC{dd} = dataSACC{numTargetSACC(dd)}.describe.date;
-    dateSACCPrimary1{dd} = dataSACCPrimary1{numTargetSACCPrimary(1,dd)}.describe.date;
-    dateSACCPrimary2{dd} = dataSACCPrimary2{numTargetSACCPrimary(2,dd)}.describe.date;
-    dateSACCPrimary3{dd} = dataSACCPrimary3{numTargetSACCPrimary(3,dd)}.describe.date;
+    dateSACC{dd} = dataSACC{numTargetDataSACC(dd)}.describe.date;
+    dateSACCPrimary1{dd} = dataSACCPrimary1{numTargetDataSACCPrimary(1,dd)}.describe.date;
+    dateSACCPrimary2{dd} = dataSACCPrimary2{numTargetDataSACCPrimary(2,dd)}.describe.date;
+    dateSACCPrimary3{dd} = dataSACCPrimary3{numTargetDataSACCPrimary(3,dd)}.describe.date;
 end
 
+%% Plot it.
+%
+% Set wavelength range.
 S = [380 2 201];
 wls = SToWls(S);
 
-%% Plot it.
 figure; hold on;
-lineColor = {'r--','k-'};
+lineColor = {'r--', 'k-'};
 lineWidth = 1;
 fontSizeTitle = 15;
 fontSizeAxis = 12;
@@ -55,25 +71,41 @@ fontSizeAxis = 12;
 % SACC
 subplot(4,1,1); hold on;
 for dd = 1:nData
-    plot(wls,spdSACC{dd},lineColor{dd},'linewidth',lineWidth);
+    plot(wls,spdSACC{dd},lineColor{dd},'linewidth',lineWidth,'DisplayName',num2str(dd));
 end
-xlabel('Wavelength (nm)', 'fontsize', fontSizeAxis);
+xlabel('Wavelength (nm)','fontsize',fontSizeAxis);
 ylabel('Spectral power','fontsize', fontSizeAxis);
 title('SACC','fontsize',fontSizeTitle);
-legend(dateSACC);
+
+% Add legend with date.
+f = get(gca, 'Children');
+if (nData == 1)
+    legend([f(1)], dateSACC{1});
+elseif (nData == 2)
+    legend([f(1) f(4)], {dateSACC{2}, dateSACC{1}});
+end
 
 % SACC primaries
 for pp = 1:nPrimaries
     subplot(4,1,pp+1); hold on;
-    xlabel('Wavelength (nm)','fontsize', fontSizeAxis);
-    ylabel('Spectral power','fontsize', fontSizeAxis);
-    title(append('SACC Primary ',num2str(pp)),'fontsize',fontSizeTitle);
     
     for dd = 1:nData
         spdTemp = eval(append('spdPrimary',num2str(pp)));
-        plot(wls,spdTemp{dd},lineColor{dd},'linewidth',lineWidth);
+        plot(wls,spdTemp{dd},lineColor{dd},'linewidth',lineWidth,'DisplayName',num2str(dd));
     end
-    legend(eval(append('dateSACCPrimary',num2str(pp))));
+    
+    xlabel('Wavelength (nm)','fontsize',fontSizeAxis);
+    ylabel('Spectral power','fontsize',fontSizeAxis);
+    title(append('SACC Primary ',num2str(pp)),'fontsize',fontSizeTitle);
+    
+    % Add legend with date.
+    f = get(gca, 'Children');
+    tempDateSACCPrimary = eval(append('dateSACCPrimary',num2str(pp)));
+    if (nData == 1)
+        legend([f(1)], tempDateSACCPrimary{1});
+    elseif (nData == 2)
+        legend([f(1) f(17)], {tempDateSACCPrimary{2}, tempDateSACCPrimary{1}});
+    end
 end
 
 %% Back to current dir.
