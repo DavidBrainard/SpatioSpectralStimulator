9b06fca467a1c125f536f2557aa006b7e71635ea
Semin-Oh <ohsemin1990@gmail.com>
2022-10-14 11:42:48 -0400

    (SEMIN) Added SACCSFA Desktop as a user in local hook

--- configuration/SpatioSpectralStimulatorLocalHookTemplate.m
+++ configuration/SpatioSpectralStimulatorLocalHookTemplate.m
@@ -33,12 +33,18 @@ switch (sysInfo.localHostName)
     case 'eagleray'
         % DHB's desktop
         baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
- 
+        
     otherwise
         % Some unspecified machine, try user specific customization
         switch(sysInfo.userShortName)
             % Could put user specific things in, but at the moment generic
             % is good enough.
+     
+            case 'colorlab'
+                % SACCSFA desktop (Linux)
+                userNameDropbox = 'Mela Nopsin';
+                baseDir = fullfile('/home/',sysInfo.userShortName,'Aguirre-Brainard Lab Dropbox',userNameDropbox);
+                
             otherwise
                 baseDir = fullfile('/Users/',sysInfo.userShortName,'Dropbox (Aguirre-Brainard Lab)');
         end
