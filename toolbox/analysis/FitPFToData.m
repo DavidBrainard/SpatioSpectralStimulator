18a482af16f3c92a21e56fb28a44fa2fcc41bb91
David Brainard <brainard@psych.upenn.edu>
2022-11-14 18:01:32 -0500

    Documentation and plot improvements for bootstrapping

--- toolbox/analysis/FitPFToData.m
+++ toolbox/analysis/FitPFToData.m
@@ -1,8 +1,13 @@
-function [paramsFitted] = FitPFToData(stimLevels,pCorrect,options)
+function [paramsFitted, ...
+    thresholdFitted, medianThresholdBoot,lowThresholdBoot,highThresholdBoot, ...
+    slopeFitted,medianSlopeBoot,lowSlopeBoot,highSlopeBoot, ...
+    legendHandles] = FitPFToData(stimLevels,pCorrect,options)
 % Fit Psychometric function to the given data.
 %
 % Syntax:
-%    [paramsFitted] = FitPFToData(stimLevels,pCorrect)
+%    [paramsFitted,...
+%      thresholdFitted, medianThresholdBoot,lowThresholdBoot,highSlopeBoot ...
+%      slopeFitted,medianSlopeBoot,lowSlopeBoot,highSlopeBoot] = FitPFToData(stimLevels,pCorrect)
 %
 % Description:
 %    This fits Psychometric function to the data given as an input. You can
@@ -19,6 +24,16 @@ function [paramsFitted] = FitPFToData(stimLevels,pCorrect,options)
 %                                 the format of the array [threshold slope
 %                                 guess lapse]. You can choose which one to
 %                                 be free/not free parameters.
+%    thresholdFitted -            Threshold at criterion
+%    medianThresholdBoot -        Median of boostrapped threshold
+%    lowThresholdBoot -           Low end of bootstrapped CI
+%    highSlopeBoot -              High end of bootstrapped CI
+%    slopeFitted -                Fit slope
+%    medianSlopeBoot -            Median of bootstrapped slopes
+%    lowSlopeBoot -               Low end of bootstrapped slope CI
+%    highSlopeBoot -              High end of bootstrapped slope CI
+%    legendHandles -              Vector of plot handles so calling routine
+%                                 can make a sensible legend.
 %
 % Optional key/value pairs:
 %    PF -                         Default to 'weibull'. Choose the function
@@ -29,6 +44,14 @@ function [paramsFitted] = FitPFToData(stimLevels,pCorrect,options)
 %                                 [threshold slope guess lapse]. Each can
 %                                 be set either 0 or 1, where 0 = fixed
 %                                 1 = free.
+%    beta -                       Set of slope parameters to fit for.  Each
+%                                 of these is fixed and the best fit over
+%                                 the set is used.  This allows us to
+%                                 control the slope range. When this is not
+%                                 empty, the second entry of params free
+%                                 above is ignored, and the fit is done for
+%                                 the passed set with best returned.
+%                                 Default is empty.
 %    nTrials -                    Default to 20. The number of trials
 %                                 conducted per each stimulus level.
 %    thresholdCriterion -         Default to 0.81606. This is the value of
@@ -52,7 +75,11 @@ function [paramsFitted] = FitPFToData(stimLevels,pCorrect,options)
 %                                 code from the Watson QUEST+ paper.
 %    addLegend                  - Default to true. Add legend when it sets
 %                                 to true.
-%    verbose -                    Default to true. Boolean. Controls
+%    nBootstraps                - Number of bootstraps to run.  0 means no
+%                                 bootstrapping. Default 0.
+%    bootConfInterval           - Size of bootstrapped confidence interval.
+%                                 Default 0.8.
+%    verbose -                  - Default to true. Boolean. Controls
 %                                 plotting and printout.
 %
 % See also:
@@ -63,6 +90,7 @@ function [paramsFitted] = FitPFToData(stimLevels,pCorrect,options)
 %   03/14/22 smo                  Added a plotting option to make different
 %                                 marker size over different number of
 %                                 trials.
+%    11/14/22  dhb          - Bootstrapping
 
 %% Set parameters.
 arguments
@@ -70,7 +98,7 @@ arguments
     pCorrect
     options.PF = 'weibull'
     options.paramsFree (1,4) = [1 1 0 1]
-    options.beta = 10.^linspace(log10(0.1),log10(10),20)
+    options.beta = []
     options.nTrials (1,1) = 20
     options.thresholdCriterion (1,1) = 0.81606
     options.newFigureWindow (1,1) = true
@@ -78,6 +106,8 @@ arguments
     options.axisLog (1,1) = true
     options.questPara = []
     options.addLegend (1,1) = true
+    options.nBootstraps = 0
+    options.bootConfInterval = 0.8
     options.verbose (1,1) = true
 end
 
@@ -102,39 +132,42 @@ end
 nTrialsPerContrast = options.nTrials * ones(size(stimLevels));
 nCorrect = round(pCorrect .* nTrialsPerContrast);
 
-% Set an initinal search parameters.
-% threshold = mean(stimLevels);
-% slope = 2;
-% guess = 0.5;
-% lapse = 0.01;
-% searchGrid = [threshold slope guess lapse];
-
+% Set an initial search parameters, with
+% gridded slope (aka beta).  The grid search
+% is only done if options.beta is empty.
 searchGrid.alpha = mean(stimLevels);  
-searchGrid.beta = options.beta;
+searchGrid.beta = 10.^(-2:0.01:2);
 searchGrid.gamma = 0.5;
 searchGrid.lambda = 0.01;
-
 lapseLimits = [0 0.05];
 
 % PF fitting happens here. Search over passed
 % list of possible slopes
-paramsFreeUse = options.paramsFree;
-paramsFreeUse(2) = 0;
-for ss = 1:length(searchGrid.beta)
-    searchGridUse = searchGrid;
-    searchGridUse.beta = searchGrid.beta(ss);
-    [paramsFittedList(ss,:) LL(ss)] = PAL_PFML_Fit(stimLevels, nCorrect, ...
-        nTrialsPerContrast, searchGridUse, paramsFreeUse, PF, 'lapseLimits', lapseLimits);
+if (~isempty(options.beta))
+    paramsFreeUse = options.paramsFree;
+    paramsFreeUse(2) = 0;
+    for ss = 1:length(options.beta)
+        searchGridUse = searchGrid;
+        searchGridUse.beta = options.beta(ss);
+        [paramsFittedList(ss,:) LL(ss)] = PAL_PFML_Fit(stimLevels, nCorrect, ...
+            nTrialsPerContrast, searchGridUse, paramsFreeUse, PF, 'lapseLimits', lapseLimits);
+    end
+    [~,index] = max(LL);
+    paramsFitted = paramsFittedList(index,:);
+
+% Otherwise use Palamedes grid search
+else
+    paramsFitted = PAL_PFML_Fit(stimLevels, nCorrect, ...
+        nTrialsPerContrast, searchGrid, paramsFree, PF, 'lapseLimits', lapseLimits);
 end
-[~,index] = max(LL);
-paramsFitted = paramsFittedList(index,:);
 thresholdFitted = PF(paramsFitted, options.thresholdCriterion, 'inv');
+slopeFitted = paramsFitted(2);
 
 % Bootstrap fits
-options.nBootstraps = 100;
 if (options.nBootstraps > 0)
     paramsFittedBoot = zeros(options.nBootstraps,4);
     for bb = 1:options.nBootstraps
+        % Bootstrap the data
         nCorrectBoot = zeros(size(nCorrect));
         for cc = 1:length(nTrialsPerContrast)
             trialsBoot = zeros(1,nTrialsPerContrast(cc));
@@ -142,20 +175,33 @@ if (options.nBootstraps > 0)
             index = randi(nTrialsPerContrast(cc),1,nTrialsPerContrast(cc));
             nCorrectBoot(cc) = sum(trialsBoot(index));
         end
-        for ss = 1:length(searchGrid.beta)
-            searchGridUse = searchGrid;
-            searchGridUse.beta = searchGrid.beta(ss);
-            [paramsFittedList(ss,:) LL(ss)] = PAL_PFML_Fit(stimLevels, nCorrectBoot, ...
-                nTrialsPerContrast, searchGridUse, paramsFreeUse, PF, 'lapseLimits', lapseLimits);
+        
+        % Fit the bootstrap data in same way we fit actual data
+        if (~isempty(options.beta))
+            paramsFreeUse = options.paramsFree;
+            paramsFreeUse(2) = 0;
+            for ss = 1:length(options.beta)
+                searchGridUse = searchGrid;
+                searchGridUse.beta = options.beta(ss);
+                [paramsFittedList(ss,:) LL(ss)] = PAL_PFML_Fit(stimLevels, nCorrectBoot, ...
+                    nTrialsPerContrast, searchGridUse, paramsFreeUse, PF, 'lapseLimits', lapseLimits);
+            end
+            [~,index] = max(LL);
+            paramsFittedBoot(bb,:) = paramsFittedList(index,:);
+        else
+            paramsFittedBoot(bb,:) = PAL_PFML_Fit(stimLevels, nCorrectBoot, ...
+                nTrialsPerContrast, searchGrid, paramsFree, PF, 'lapseLimits', lapseLimits);
         end
-        [~,index] = max(LL);
-        paramsFittedBoot(bb,:) = paramsFittedList(index,:);
+
+        % Grab bootstrapped threshold
         thresholdFittedBoot(bb) = PF(paramsFittedBoot(bb,:), options.thresholdCriterion, 'inv');
     end
     medianThresholdBoot = median(thresholdFittedBoot);
-    lowThresholdBoot = prctile(thresholdFittedBoot,10);
-    highThresholdBoot = prctile(thresholdFittedBoot,90);
-
+    lowThresholdBoot = prctile(thresholdFittedBoot,100*(1-options.bootConfInterval)/2);
+    highThresholdBoot = prctile(thresholdFittedBoot,100-100*(1-options.bootConfInterval)/2);
+    medianSlopeBoot = median(paramsFittedBoot(:,2));
+    lowSlopeBoot = prctile(paramsFittedBoot(:,2),100*(1-options.bootConfInterval)/2);
+    highSlopeBoot = prctile(paramsFittedBoot(:,2),100-100*(1-options.bootConfInterval)/2);
 else
     paramsFittedBoot = [];
     medianThresholdBoot = [];
@@ -196,48 +242,64 @@ if (options.verbose)
     % Plot bootstraps
     for bb = 1:options.nBootstraps
         smoothPsychometricBoot = PF(paramsFittedBoot(bb,:), fineStimLevels);
-        plot(fineStimLevelsPlot,smoothPsychometricBoot,'Color',[0.9 0.8 0.8],'LineWidth',0.5);
+        h_bsfit = plot(fineStimLevelsPlot,smoothPsychometricBoot,'Color',[0.9 0.8 0.8],'LineWidth',0.5);
     end
 
     % Plot best fit here.
-    scatter(stimLevelsPlot, pCorrect, options.pointSize,...
+    h_data = scatter(stimLevelsPlot, pCorrect, options.pointSize,...
         'MarkerEdgeColor', zeros(1,3), 'MarkerFaceColor', ones(1,3) * 0.5, 'MarkerFaceAlpha', 0.5);
-    plot(fineStimLevelsPlot,smoothPsychometric,'r','LineWidth',3);
+    h_pffit = plot(fineStimLevelsPlot,smoothPsychometric,'r','LineWidth',3);
     
     % Mark the threshold point (red point).
     if(options.axisLog)
-        plot(thresholdFittedLog,options.thresholdCriterion,'ko','MarkerFaceColor','r','MarkerSize',12);
-        h = errorbarX(log10(medianThresholdBoot),options.thresholdCriterion,log10(medianThresholdBoot)-log10(lowThresholdBoot),log10(highThresholdBoot)-log10(medianThresholdBoot),'go');
-        set(h,'MarkerSize',9); set(h,'MarkerFaceColor','g'); set(h,'MarkerEdgeColor','g'); set(h,'LineWidth',3);
+        h_thresh = plot(thresholdFittedLog,options.thresholdCriterion,'ko','MarkerFaceColor','r','MarkerSize',12);
+        if (options.nBootstraps > 0)
+            h_bsthresh = errorbarX(log10(medianThresholdBoot),options.thresholdCriterion,log10(medianThresholdBoot)-log10(lowThresholdBoot),log10(highThresholdBoot)-log10(medianThresholdBoot),'go');
+            set(h_bsthresh,'MarkerSize',9); set(h_bsthresh,'MarkerFaceColor','g'); set(h_bsthresh,'MarkerEdgeColor','g'); set(h_bsthresh,'LineWidth',3);
+        end
         xlabel('Contrast (log)', 'FontSize', 15);
     else
-        plot(thresholdFitted,options.thresholdCriterion,'ko','MarkerFaceColor','r','MarkerSize',12);
-        h = errorbarX(medianThresholdBoot,options.thresholdCriterion,medianThresholdBoot-lowThresholdBoot,highThresholdBoot-medianThresholdBoot,'go');
-        set(h,'MarkerSize',9); set(h,'MarkerFaceColor','g'); set(h,'MarkerEdgeColor','g'); set(h,'LineWidth',3);
+        h_thresh = plot(thresholdFitted,options.thresholdCriterion,'ko','MarkerFaceColor','r','MarkerSize',12);
+        if (options.nBootstraps > 0)
+            h_bsthresh = errorbarX(medianThresholdBoot,options.thresholdCriterion,medianThresholdBoot-lowThresholdBoot,highThresholdBoot-medianThresholdBoot,'go');
+            set(h_bsthresh,'MarkerSize',9); set(h_bsthresh,'MarkerFaceColor','g'); set(h_bsthresh,'MarkerEdgeColor','g'); set(h_bsthresh,'LineWidth',3);
+        end
         xlabel('Contrast', 'FontSize', 15);
     end
     ylabel('pCorrect', 'FontSize', 15);
     ylim([0 1]);
-    title(append('Threshold: ', num2str(round(real(thresholdFitted),4))), 'FontSize', 15);
-    drawnow;
+    if (options.nBootstraps > 0)
+        title({sprintf('Threshold: %0.4f, [%0.4f (%0.4f, %0.4f, %d%%]',...
+            thresholdFitted,medianThresholdBoot,lowThresholdBoot,highThresholdBoot,round(100*options.bootConfInterval)) ; ...
+            sprintf('Slope: %0.4f, [%0.4f (%0.4f, %0.4f, %d%%]',...
+            slopeFitted,medianSlopeBoot,lowSlopeBoot,highSlopeBoot,round(100*options.bootConfInterval))});
+    else
+        title({sprintf('Threshold: %0.4f',thresholdFitted); sprintf('Slope: %0.4f',slopeFitted)});
+    end
     
     %% Get QuestPlus prediction and add to plot.
     %
     % Calculate QuestPlus prediction here and plot it.
     if ~isempty(options.questPara)
         predictedQuestPlus = qpPFWeibullLog(fineStimLevelsPlot',options.questPara);
-        plot(fineStimLevelsPlot,predictedQuestPlus(:,2),'k--','LineWidth',3);
+        h_quest = plot(fineStimLevelsPlot,predictedQuestPlus(:,2),'k--','LineWidth',3);
     end
     
     % Add legend if you want.
+    if ~isempty(options.questPara)
+        legendHandles = [h_data h_pffit h_thresh h_bsthresh(2) h_bsthresh(1) h_quest];
+    else
+        legendHandles = [h_data h_pffit h_thresh h_bsthresh(2) h_bsthresh(1)];
+    end
     if (options.addLegend)
         if ~isempty(options.questPara)
-            legend('Data','PF-fit','PF-Threshold','Quest-fit',...
+            legend(legendHandles, 'Data','PF-fit','PF-Threshold','BS-Threshold','BS-ConfInt', 'Quest-fit',...
                 'FontSize', 12, 'location', 'southeast');
         else
-            legend('Data','PF-fit','PF-Threshold',...
+            legend(legendHandles, 'Data','PF-fit','PF-Threshold','BS-Threshold','BS-ConfInt', ...
                 'FontSize', 12, 'location', 'southeast');
         end
     end
-    
+    drawnow;
+
 end
