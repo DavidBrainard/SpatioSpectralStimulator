a615dd12f5d7e4265ae41163e07c7a2e757d3e9a
Semin-Oh <ohsemin1990@gmail.com>
2022-11-16 10:00:39 -0500

    (SEMIN) Now checking images by eye including high contrast image

--- experiment/t_CSFGeneratorCheckTestImages.m
+++ experiment/t_CSFGeneratorCheckTestImages.m
@@ -37,3 +37,33 @@ for ss = 1:nSineFreqCyclesPerDeg
     imshow(images{ss});
     title(sprintf('%dcpd',sineFreqCyclesPerDegTemp),'fontsize',15);
 end
+
+%% Load images (high contrast).
+sineFreqCyclesPerDeg = [18];
+olderDate = 0;
+nSineFreqCyclesPerDeg = numel(sineFreqCyclesPerDeg);
+
+for ss = 1:nSineFreqCyclesPerDeg
+    sineFreqCyclesPerDegTemp = sineFreqCyclesPerDeg(ss);
+    
+    if (ispref('SpatioSpectralStimulator','SACCData'))
+        testFiledir = fullfile(getpref('SpatioSpectralStimulator','SACCData'),'TestImages');
+        testFilename = GetMostRecentFileName(testFiledir,...
+            sprintf('RunExpData_high_%d_cpd',sineFreqCyclesPerDegTemp),'olderDate',olderDate);
+        theData{ss} = load(testFilename);
+        images{ss} = theData{ss}.sceneParamsStruct.predefinedRGBImages{31};
+    else
+        error('Cannot find data file');
+    end
+    [filepath,filename,ext] = fileparts(testFilename);
+    fprintf('\t *Image data has been successfully loaded! (%d/%d) \n',ss,nSineFreqCyclesPerDeg);
+    fprintf('\t *Loaded file name = (%s) \n',filename);
+end
+
+%% Plot images.
+for ss = 1:nSineFreqCyclesPerDeg
+    sineFreqCyclesPerDegTemp = sineFreqCyclesPerDeg(ss);
+    figure; clf;
+    imshow(images{ss});
+    title(sprintf('%dcpd',sineFreqCyclesPerDegTemp),'fontsize',15);
+end
