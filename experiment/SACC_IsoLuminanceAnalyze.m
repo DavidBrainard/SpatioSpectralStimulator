dc9c9ccdd560e2c644b021fd34d340c379cfaa2e
SeminOh <ohsemin1990@gmail.com>
2022-12-06 10:25:28 -0500

    (SEMIN) Updates on analyzing flicker photom

--- experiment/SACC_IsoLuminanceAnalyze.m
+++ experiment/SACC_IsoLuminanceAnalyze.m
@@ -15,64 +15,97 @@ clear; close all;
 if (ispref('SpatioSpectralStimulator','SACCData'))
     testFiledir = getpref('SpatioSpectralStimulator','SACCData');
     testFileList = dir(testFiledir);
-
+    
     % Find available subject name.
     for tt = 1:length(testFileList)
         testFilenameList{tt}  = testFileList(tt).name;
     end
     idxSubjectName = find(str2double(testFilenameList)>0);
     subjectNameOptions = testFilenameList(idxSubjectName);
-
+    
     % Find subjects that has iso-luminance determination data.
     nSubjectName = length(subjectNameOptions);
     numSubjects = {};
     allData = {};
-
+    allCalData = {};
+    
     for ss = 1:nSubjectName
         testFiledirTemp = fullfile(testFiledir,subjectNameOptions{ss},'FlickerPhotom');
         subjectNameTemp = subjectNameOptions{ss};
-
+        
         % Load the data here if available.
         if isdir(testFiledirTemp)
             testFilenameTemp = GetMostRecentFileName(testFiledirTemp,...
                 sprintf('flickerPhotom_%s',subjectNameTemp));
             data = load(testFilenameTemp);
-
+            
+            % Load the calibration file used for the experiment.
+            [dir filename format] = fileparts(testFilenameTemp);
+            numExtract = regexp(filename,'\d+','match');
+            date = sprintf('%s-%s-%s',numExtract{2},numExtract{3},numExtract{4});
+            
+            calFiledir = fullfile(testFiledir,'CheckFlickerPhotom');
+            calDataFilename = GetMostRecentFileName(calFiledir,...
+                sprintf('checkFlickerPhotom_%s',date));
+            calData = load(calDataFilename);
+        
             % Save subject name and the data.
             numSubjects{end+1} = subjectNameTemp;
             allData{end+1} = data.data;
+            allCalData{end+1} = calData;
         end
     end
 end
 
 %% Plot it.
 figure; clf; hold on;
-xlabel('Subject name');
-ylabel('Intensity matching red (8-bit)');
+fontSize = 15;
+xlabel('Subject name','fontsize',fontSize);
+ylabel('Output (no unit)','fontsize',fontSize);
 xlim([1.5 str2double(numSubjects{end})])
-ylim([56 180]);
+ylim([0 1.2]);
+xticks([str2double(numSubjects{1}) : 1 : str2double(numSubjects{end})]);
 
 nSubjects = length(numSubjects);
 for ss = 1:nSubjects
     numSubject = str2double(numSubjects{ss});
     
     if (numSubject == 2)
-        dataRaw = [92 104 116 104 100 112];
+        redInputSettings = [92 104 116 104 100 112];
     else
-        dataRaw = allData{ss}.results;
+        redInputSettings = allData{ss}.results;
     end
-
-    dataMean = mean(dataRaw);
-    dataStd = std(dataRaw);
-
-    % Plot raw data.
-    plot(numSubject*ones(1,length(dataRaw)), dataRaw,'ko');
-
-    % Plot mean and its standard deviation.
-    plot(numSubject, dataMean,'ro','markerfacecolor','r');
-    errorbar(numSubject, dataMean, dataStd, 'r');
-
-    clear dataRaw;
+    
+    % Linear fitting to get output settings of the matching results (no unit).
+    x = allCalData{ss}.data.redIntensity;
+    y = allCalData{ss}.dataBC.xyzRedBC(2,:);
+    p = polyfit(x, y, 1);
+    redOutputSettings = polyval(p, redInputSettings);
+    greenOutputSetting = allCalData{ss}.dataBC.xyzGreenBC(2);
+    
+    % Calculate mean and std of the output settings.
+    meanRedOutputSettings = mean(redOutputSettings);
+    stdRedOutputSettings = std(redOutputSettings);
+    
+    % Plot raw data (Red).
+    plot(numSubject*ones(1,length(redOutputSettings)), redOutputSettings,'ko');
+    
+    % Plot the reference (Green).
+    plot(numSubject, greenOutputSetting,'go','markersize',9,'markeredgecolor','k','markerfacecolor','g');
+    
+    % Plot mean and its standard deviation (Red).
+    plot(numSubject, meanRedOutputSettings,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
+    errorbar(numSubject, meanRedOutputSettings, stdRedOutputSettings, 'r', 'linewidth', 1);
 end
+legend('Raw data (red)','Reference (green)','Mean (red)','fontsize',fontSize,'location','southeast');
 
 %% Save the plot.
+SAVETHEPLOT = true;
+if (SAVETHEPLOT)
+    if (ispref('SpatioSpectralStimulator','SACCAnalysis'))
+        testFiledir = fullfile(getpref('SpatioSpectralStimulator','SACCAnalysis'));
+        testFilename = fullfile(testFiledir,'Results_FlickerPhotom');
+        testFileFormat = '.tiff';
+        saveas(gcf,append(testFilename,testFileFormat));
+    end
+end
