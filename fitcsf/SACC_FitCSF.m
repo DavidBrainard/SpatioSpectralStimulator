dc9dac797b1e13b090ffbcba05fa23e91cb34cd9
SeminOh <ohsemin1990@gmail.com>
2023-01-13 11:56:01 -0500

    (SEMIN) Starting on the script for fitting CSF

---
+++ fitcsf/SACC_FitCSF.m
@@ -0,0 +1,35 @@
+%% SACC_FitCSF
+%
+% This is to fit CSF curve for SACC project.
+%
+% See also:
+%    asymmetricParabolicFunc
+
+% History:
+%    1/13/23   smo    - Started on it.
+
+%% Initialize.
+clear; close all;
+
+%% Load the data.
+
+
+%% Set variables.
+myCSVals = [];
+mySFVals = [3 6 9 12 18];
+myWs = 1/[error or CIs];
+
+%% Optimize the parameter p using fmincon.
+myObjs = norm(myWs .* (myCSVals - asymmetricParabolicFunc(p, mySFVals)));
+p_lowerBound = [10 0.5 0.1 0.1];
+p_higherBound = [500 18 10 10];
+
+p = fmincon(myObjs, p_lowerBound, p_higherBound);
+
+%% Fitting.
+
+
+%% Plot it.
+
+
+%% Save the results.
