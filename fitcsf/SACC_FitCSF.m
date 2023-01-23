090e7bcc187c591350cc698e75945aab488e7578
Semin <seminoh@sas.upenn.edu>
2023-01-23 07:59:22 -0500

    (SEMIN) Added an option to fit one by one or all at once

--- fitcsf/SACC_FitCSF.m
+++ fitcsf/SACC_FitCSF.m
@@ -13,12 +13,16 @@
 %% Initialize.
 clear; close all;
 
+%% Set the options.
+DRAWONEFIGUREPERSUB = true;
+WAITFORKEYTODRAW = true;
+
 %% Load the data.
 if (ispref('SpatioSpectralStimulator','SACCAnalysis'))
     testFiledir = getpref('SpatioSpectralStimulator','SACCAnalysis');
     testFilename = fullfile(testFiledir,'CSFAnalysisOutput');
     theData = load(testFilename);
-    
+
     % Close the plots if popping up any.
     close all;
 else
@@ -43,39 +47,50 @@ nFilters = length(theData.filterOptions);
 for ss = 1:nSubjects
     %% Set target subject and filter. We will fit CSF one by one.
     subjectName = theData.subjectNameOptions{ss};
-    
+
     % Set available spatial frequency data for the subject.
     sineFreqCyclesPerDeg = theData.spatialFrequencyOptions(:,ss);
-    
+
     % Here we remove empty cells. It shows a cell empty when a subject does
     % not have all possible spatial frequency data.
     sineFreqCyclesPerDeg = sineFreqCyclesPerDeg(...
         find(~cellfun(@isempty,sineFreqCyclesPerDeg)));
-    
+
     nSineFreqCyclesPerDeg = length(sineFreqCyclesPerDeg);
-    
+
     % Run fitting only if there are all spatial frequency data.
     if (nSineFreqCyclesPerDeg == 5)
         % Get spatial frequency data in double.
         for dd = 1:nSineFreqCyclesPerDeg
             sineFreqCyclesPerDegNum(dd) = sscanf(sineFreqCyclesPerDeg{dd},'%d');
         end
-        
+
         %% Make a new plot per each subject.
-        figure; clf; hold on;
-        figureSize = 900;
-        figurePosition = [1000 500 figureSize figureSize];
-        set(gcf,'position',figurePosition);
-        
+        if (DRAWONEFIGUREPERSUB)
+            figure; clf; hold on;
+            figureSize = 800;
+            figurePosition = [1000 1000 figureSize figureSize];
+            set(gcf,'position',figurePosition);
+        end
+
         % Here we read out five values of the thresholds (so, five spatial
         % frequency) to fit CSF curve.
         for ff = 1:nFilters
+
+            % Make a new plot per each filter of the subject.
+            if (~DRAWONEFIGUREPERSUB)
+                figure; clf; hold on;
+                figureSize = 550;
+                figurePosition = [100 100 figureSize figureSize];
+                set(gcf,'position',figurePosition);
+            end
+
             % Each variable should have the number of 5 entries.
             thresholdsRaw = thresholdFittedRaw(ss,:,ff);
             thresholdsBoot = medianThresholdBootRaw(ss,:,ff);
             thresholdBootLow = lowThresholdBootRaw(ss,:,ff);
             thresholdBootHigh = highThresholdBootRaw(ss,:,ff);
-            
+
             % Convert NaN to 0 here.
             for tt = 1:length(thresholdsRaw)
                 if isnan(thresholdsRaw(tt))
@@ -87,50 +102,50 @@ for ss = 1:nSubjects
                     thresholdsBoot(tt) = 0;
                 end
             end
-            
+
             %% Calculate sensitivity.
             sensitivityRawLinear = 1./thresholdsRaw;
             sensitivityRawLog = log10(sensitivityRawLinear);
-            
+
             sensitivityBootLinear = 1./thresholdsBoot;
             sensitivityBootLog = log10(sensitivityBootLinear);
-            
+
             % For calculation of confindence interval from bootstrap,
             % (low) threshold becomes (high) sensitivity, and vice
             % versa.
             sensitivityBootHighLinear = 1./thresholdBootLow;
             sensitivityBootHighLog = log10(sensitivityBootHighLinear);
-            
+
             sensitivityBootLowLinear = 1./thresholdBootHigh;
             sensitivityBootLowLog = log10(sensitivityBootLowLinear);
-            
+
             % Calculate spatial frequency in log space.
             sineFreqCyclesPerDegLog = log10(sineFreqCyclesPerDegNum);
-            
+
             % Sort each array in an ascending order of spatial
             % frequency.
             [sineFreqCyclesPerDegNumSorted I] = sort(sineFreqCyclesPerDegNum,'ascend');
             sineFreqCyclesPerDegLogSorted = sineFreqCyclesPerDegLog(I);
-            
+
             % Linear sorted according to spatial frequency.
             sensitivityRawLinearSorted = sensitivityRawLinear(I);
             sensitivityBootLinearSorted = sensitivityBootLinear(I);
             sensitivityBootHighLinearSorted = sensitivityBootHighLinear(I);
             sensitivityBootLowLinearSorted = sensitivityBootLowLinear(I);
-            
+
             % Log sorted according to spatial frequency.
             sensitivityRawLogSorted = sensitivityRawLog(I);
             sensitivityBootLogSorted = sensitivityBootLog(I);
             sensitivityBootHighLogSorted = sensitivityBootHighLog(I);
             sensitivityBootLowLogSorted = sensitivityBootLowLog(I);
-            
+
             %% Fitting happens here.
             %
             % Set variables to fit CSF.
             mySFVals = sineFreqCyclesPerDegNumSorted;
             myCSVals = sensitivityRawLinearSorted;
-            myWs = 1;
-            
+            myWs = 1./(sensitivityBootHighLinearSorted-sensitivityBootLowLinearSorted);
+
             % Set parameters for optimazation of the parameter p.
             p0 = [10 0.5 0.1 0.1];
             p_lowerBound = [10 0.5 0.1 0.1];
@@ -140,11 +155,11 @@ for ss = 1:nSubjects
             Aeq = []; % Matrix for linear equality constraints
             beq = []; % Vector for linear equality constraints
             options = optimset('fmincon');
-            
+
             % Optimize p here.
             p_optimized = fmincon(@(p_unknown) norm(myWs .* (myCSVals - asymmetricParabolicFunc(p_unknown, mySFVals))), ...
                 p0, A, b, Aeq, beq, p_lowerBound, p_higherBound, [], options);
-            
+
             %% Plot it here.
             %
             % Different marker/line options for raw fit and bootstrap
@@ -153,11 +168,11 @@ for ss = 1:nSubjects
             colorOptionsBoot = {'k+','r+','g+','b+','c+'};
             colorOptionsCSF = {'k-','r-','g-','b-','c-'};
             colorOptionsCI = {'k','r','g','b','c'};
-            
+
             % Plot raw data.
             plot(sineFreqCyclesPerDegNumSorted, sensitivityRawLinearSorted, colorOptionsRaw{ff},'markersize',20);
             plot(sineFreqCyclesPerDegNumSorted, sensitivityBootLinearSorted, colorOptionsBoot{ff},'markersize',20);
-            
+
             % Plot the CSF fitting results.
             SF_CSF_start = 3;
             SF_CSF_end = 18;
@@ -165,44 +180,96 @@ for ss = 1:nSubjects
             sineFreqCyclesPerDegNumCSF = linspace(SF_CSF_start,SF_CSF_end,nPointsCSF);
             sensitivityCSFLinear = asymmetricParabolicFunc(p_optimized, sineFreqCyclesPerDegNumCSF);
             plot(sineFreqCyclesPerDegNumCSF, sensitivityCSFLinear, colorOptionsCSF{ff}, 'linewidth', 2);
-            
+
             % Plot Confidence Interval acquired from Bootstrap.
             errorNeg = abs(sensitivityBootLinearSorted - sensitivityBootLowLinearSorted);
             errorPos = abs(sensitivityBootHighLinearSorted - sensitivityBootLinearSorted);
             errorbar(sineFreqCyclesPerDegNumSorted, sensitivityBootLinearSorted, ...
                 errorNeg, errorPos, colorOptionsCI{ff});
+
+            % Add details per each plot of the subject.
+            if (~DRAWONEFIGUREPERSUB)
+                xlabel('Spatial Frequency (cpd)','fontsize',15);
+                ylabel('Contrast Sensitivity (Linear unit)','fontsize',15);
+
+                xticks(sineFreqCyclesPerDegNumSorted);
+                xticklabels(sineFreqCyclesPerDegNumSorted);
+
+                yaxisRangeLinear = [0:50:300];
+                ylim([0 300]);
+                yticks(yaxisRangeLinear);
+                yticklabels(yaxisRangeLinear);
+
+                title(sprintf('CSF curve - Sub %s',subjectName),'fontsize',15);
+
+                % Add legend.
+                f = flip(get(gca, 'Children'));
+
+                numSpaceLegend = 4;
+                idxLegendRaw = linspace(1, 1+numSpaceLegend*(nSineFreqCyclesPerDeg-1), nSineFreqCyclesPerDeg);
+                idxLegendBoot = linspace(2, 2+numSpaceLegend*(nSineFreqCyclesPerDeg-1), nSineFreqCyclesPerDeg);
+
+                filterWls = {'392 nm' '417 nm' '437 nm' '456 nm' '476 nm'};
+                for ll = 1:nSineFreqCyclesPerDeg
+                    contentLegendRaw{ll} = sprintf('%s (%s) -PF',theData.filterOptions{ll}, filterWls{ll});
+                    contentLegendBoot{ll} = sprintf('%s (%s) -Boot',theData.filterOptions{ll}, filterWls{ll});
+                end
+
+                % Add legend when drawing one figure per each filter.
+                legend(append(filterWls{ff},'-PF'),append(filterWls{ff},'-Boot'),...
+                    'fontsize', 13, 'location', 'northeast');
+            end
+
+            % Key stroke to start measurement.
+            if (~DRAWONEFIGUREPERSUB)
+                if (WAITFORKEYTODRAW)
+                    disp('Press a key to draw next plot!');
+                    pause;
+                    close;
+                end
+            end
+        end
+
+        %% Add details per each plot of the subject.
+        if (DRAWONEFIGUREPERSUB)
+            xlabel('Spatial Frequency (cpd)','fontsize',15);
+            ylabel('Contrast Sensitivity (Linear unit)','fontsize',15);
+
+            xticks(sineFreqCyclesPerDegNumSorted);
+            xticklabels(sineFreqCyclesPerDegNumSorted);
+
+            yaxisRangeLinear = [0:50:300];
+            ylim([0 300]);
+            yticks(yaxisRangeLinear);
+            yticklabels(yaxisRangeLinear);
+
+            title(sprintf('CSF curve - Sub %s',subjectName),'fontsize',15);
+
+            % Add legend.
+            f = flip(get(gca, 'Children'));
+
+            numSpaceLegend = 4;
+            idxLegendRaw = linspace(1, 1+numSpaceLegend*(nSineFreqCyclesPerDeg-1), nSineFreqCyclesPerDeg);
+            idxLegendBoot = linspace(2, 2+numSpaceLegend*(nSineFreqCyclesPerDeg-1), nSineFreqCyclesPerDeg);
+
+            filterWls = {'392 nm' '417 nm' '437 nm' '456 nm' '476 nm'};
+            for ll = 1:nSineFreqCyclesPerDeg
+                contentLegendRaw{ll} = sprintf('%s (%s) -PF',theData.filterOptions{ll}, filterWls{ll});
+                contentLegendBoot{ll} = sprintf('%s (%s) -Boot',theData.filterOptions{ll}, filterWls{ll});
+            end
+
+            % Add legend when drawing one figure per each subject.
+            legend(f([idxLegendRaw idxLegendBoot]), [contentLegendRaw contentLegendBoot], ...
+                'fontsize', 13, 'location', 'northeast');
+
+            % Key stroke to start measurement.
+            if (WAITFORKEYTODRAW)
+                disp('Press a key to draw next plot!');
+                pause;
+                close;
+            end
         end
     end
-    
-    %% Add details per each plot of the subject.
-    xlabel('Spatial Frequency (cpd)','fontsize',15);
-    ylabel('Contrast Sensitivity (Linear unit)','fontsize',15);
-    
-    xticks(sineFreqCyclesPerDegNumSorted);
-    xticklabels(sineFreqCyclesPerDegNumSorted);
-    
-    yaxisRangeLinear = [0:50:300];
-    ylim([0 300]);
-    yticks(yaxisRangeLinear);
-    yticklabels(yaxisRangeLinear);
-    
-    title(sprintf('CSF curve - Sub %s',subjectName),'fontsize',15);
-    subtitle('Data points are in log unit, labels are in linear unit', 'fontsize',13);
-    
-    % Add legend.
-    f = flip(get(gca, 'Children'));
-    
-    numSpaceLegend = 3;
-    idxLegendRaw = linspace(1, 1+numSpaceLegend*(nSineFreqCyclesPerDeg-1), nSineFreqCyclesPerDeg);
-    idxLegendBoot = linspace(2, 2+numSpaceLegend*(nSineFreqCyclesPerDeg-1), nSineFreqCyclesPerDeg);
-    
-    filterWls = {'392 nm' '417 nm' '437 nm' '456 nm' '476 nm'};
-    for ll = 1:nSineFreqCyclesPerDeg
-        contentLegendRaw{ll} = sprintf('%s (%s) -PF',theData.filterOptions{ll}, filterWls{ll});
-        contentLegendBoot{ll} = sprintf('%s (%s) -Boot',theData.filterOptions{ll}, filterWls{ll});
-    end
-    legend(f([idxLegendRaw idxLegendBoot]), [contentLegendRaw contentLegendBoot], ...
-        'fontsize', 13, 'location', 'northeast');
 end
 
 %% Save the results.
