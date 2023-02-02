3223383847d337426db0a2310069743e0709ed7d
SeminOh <ohsemin1990@gmail.com>
2023-02-02 14:17:59 -0500

    (SEMIN) MPOD plot legends updated

--- experiment/SACC_MPODAnalyze.m
+++ experiment/SACC_MPODAnalyze.m
@@ -3,7 +3,8 @@
 % This is for analyzing MPOD measurement results.
 
 % History:
-%    12/5/22   smo    - Started on it.
+%    12/05/22   smo    - Started on it.
+%    02/02/23   smo    - Updated the legends on the plot.
 
 %% Initialize.
 clear; close all;
@@ -12,17 +13,17 @@ clear; close all;
 if (ispref('SpatioSpectralStimulator','SACCMaterials'))
     testFiledir = fullfile(getpref('SpatioSpectralStimulator','SACCMaterials'),'SACC_MPOD');
     testFileList = dir(testFiledir);
-
+    
     % Find available subject name.
     for tt = 1:length(testFileList)
         testFilenameList{tt}  = testFileList(tt).name;
     end
     idxSubjectName = find(str2double(testFilenameList)>0);
     subjectNameOptions = testFilenameList(idxSubjectName);
-
+    
     % Read MPOD data here.
     nSubjectName = length(subjectNameOptions);
-
+    
     for ss = 1:nSubjectName
         % Set the directory of the file saved.
         testFiledirTemp = fullfile(testFiledir,subjectNameOptions{ss});
@@ -32,7 +33,7 @@ if (ispref('SpatioSpectralStimulator','SACCMaterials'))
         numTargetFilename = 3;
         testFilenameTemp = fullfile(testFiledirTemp,fileListTemp(numTargetFilename).name);
         data = readtable(testFilenameTemp);
-
+        
         % Get the info of which eye was first measured.
         whichEyeFirstMeasure = data{3,2}{1};
         
@@ -63,7 +64,7 @@ if (ispref('SpatioSpectralStimulator','SACCMaterials'))
             stdLeftEye(ss) = stdFirstMeasure;
             mpodRightEye(ss) = mpodSecondMeasure;
             stdRightEye(ss) = stdSecondMeasure;
-
+            
         elseif (strcmp(whichEyeFirstMeasure,'R'))
             mpodLeftEye(ss) = mpodSecondMeasure;
             stdLeftEye(ss) = stdSecondMeasure;
@@ -81,63 +82,59 @@ ylabel('MPOD', 'fontSize', fontSize);
 xlim([1.5 str2double(subjectNameOptions{end})])
 xticks([str2double(subjectNameOptions{1}) : 1 : str2double(subjectNameOptions{end})]);
 title('MPOD results','fontsize',fontSize);
-subtitle('Dominant eye is marked as triangle and big','fontsize',fontSize);
+subtitle('Left eye = (triangle) / right eye = (circle)','fontsize',fontSize);
 
 for ss = 1:nSubjectName
     numSubject = subjectNameOptions(ss);
     numSubject = str2double(numSubject);
-
+    
     % For the plot, we will set different marker for dominant and
     % non-dominant eyes.
     %
-    % Marker type.
-    markerTypeDominantEye = '^';
-    markerTypeNonDominantEye = 'o';
+    % Marker type (left and right).
+    markerTypeLeftEye = '^';
+    markerTypeRightEye = 'o';
     
-    % Marker size.
-    markerSizeDominantEye = 13;
-    markerSizeNonDominantEye = 5;
+    % Marker size (dom or non-dom).
+    markerSizeDominantEye = 7;
+    markerSizeNonDominantEye = 7;
     
-    % Marker color.
-    markerColorDominantEye = 'c';
+    % Marker color (dom or non-dom).
+    markerColorDominantEye = 'r';
     markerColorNonDominantEye = 'k';
     
     % Set marker styles here.
     if strcmp(whichEyeDominant{ss},'L')
-        markerTypeLeftEye = markerTypeDominantEye;
-        markerTypeRightEye = markerTypeNonDominantEye;
-        
         markerSizeLeftEye = markerSizeDominantEye;
         markerSizeRightEye = markerSizeNonDominantEye;
         
         markerColorLeftEye = markerColorDominantEye;
         markerColorRightEye = markerColorNonDominantEye;
     else
-        markerTypeLeftEye = markerTypeNonDominantEye;
-        markerTypeRightEye = markerTypeDominantEye;
-
         markerSizeLeftEye = markerSizeNonDominantEye;
         markerSizeRightEye = markerSizeDominantEye;
-
-        markerColorLeftEye = markerColorDominantEye;
-        markerColorRightEye = markerColorNonDominantEye;
+        
+        markerColorLeftEye = markerColorNonDominantEye;
+        markerColorRightEye = markerColorDominantEye;
     end
     
     % Plot mpod and its standard deviation.
     %
     % Left eye.
-    plot(numSubject, mpodLeftEye(ss), markerTypeLeftEye,'markersize',markerSizeLeftEye,'markeredgecolor','k','markerfacecolor','b');
-    errorbar(numSubject, mpodLeftEye(ss), stdLeftEye(ss), 'b','linewidth',1);
-
-    % Right eye. 
-    plot(numSubject, mpodRightEye(ss),markerTypeRightEye,'markersize',markerSizeRightEye,'markeredgecolor','k','markerfacecolor','c');
-    errorbar(numSubject, mpodRightEye(ss), stdRightEye(ss), 'c','linewidth',1);
-
+    plot(numSubject, mpodLeftEye(ss), markerTypeLeftEye,'markersize',markerSizeLeftEye,...
+        'markeredgecolor','k','markerfacecolor',markerColorLeftEye);
+    errorbar(numSubject, mpodLeftEye(ss), stdLeftEye(ss), markerColorLeftEye,'linewidth',1);
+    
+    % Right eye.
+    plot(numSubject, mpodRightEye(ss), markerTypeRightEye,'markersize',markerSizeRightEye,...
+        'markeredgecolor','k','markerfacecolor',markerColorRightEye);
+    errorbar(numSubject, mpodRightEye(ss), stdRightEye(ss), markerColorRightEye,'linewidth',1);
+    
     % Connect data points between left and right eyes.
     plot([numSubject numSubject], [mpodLeftEye(ss) mpodRightEye(ss) ],'k:','LineWidth',1);
     
 end
-legend('Left eye (color)','','Right eye (color)', 'fontSize', fontSize)
+legend('Dominant eye (red)','','Non-dominant eye (black)', 'fontSize', fontSize)
 
 %% Save the plot.
 SAVETHEPLOT = true;
