5d274572fed6a134c4e88e28688db522368d3ee7
SeminOh <ohsemin1990@gmail.com>
2023-03-21 10:11:37 -0400

    (SEMIN) Set the function in the format, but still need to be cleaned up

--- toolbox/analysis/SmoothnessSearchErrorFunction.m
+++ toolbox/analysis/SmoothnessSearchErrorFunction.m
@@ -1,20 +1,45 @@
-% This function computes the cross validated error.  This is really just
-% computing the same thing you are already plotting in your grid search
-% over smoothness parameter values, placed into a function that fmincon
-% understands.
-function [fitError] = SmoothnessSearchErrorFunction(x, csfSFs, fitData, evaluateData)
-% Grab current smoothness param from parameter vector.
-smoothingParam = x(1);
+function [fitError] = SmoothnessSearchErrorFunction(smoothingParam, csfSFs, fitData, evaluateData)
+% It computes the cross validated error when fitting smoothing spline
+% function.
+%
+% Syntax:
+%    [fitError] = SmoothnessSearchErrorFunction(x, csfSFs, fitData, evaluateData)
+%
+% Description:
+%    This is computing the fitting error in the same way that we did in our
+%    grid search over smoothing parameter values. It is in the form of
+%    function so that fmincon understands.
+%
+% Inputs:
+%    smoothingParam
+%    csfSFs
+%    fitData
+%    evaluateData
+%
+% Outputs:
+%    fitError
+%
+% Optional key/value pairs.
+%    verbose
+%
+% See also:
+%    SACC_FitCSF.
 
-% Use the spline to fit the fit data, for each cross
-% valiation iteration Each fit/evaluate csf pair in a
-% column of fitData/evaluteData.
+% History:
+%   03/17/23  dhb, smo         - Started on it.
+%   03/20/23  smo              - Made it as a separate function.
+
+%% Get the number of spatial frequency and data to fit.
 nSFs = length(csfSFs);
 nFits = size(fitData,2);
-fitError = 0;
 
-% Use spline to fit the data with passed smoothing param.
+%% Fitting happens here.
+%
+% Use the spline to fit the fit data, for each cross valiation iteration.
+% Each fit/evaluate csf pair in a column of fitData/evaluteData.
+fitError = 0;
 for ii = 1:nFits
+    % Fit here and get predicted data.
     smoothFit = fit(csfSFs',fitData{ii}','smoothingspline','SmoothingParam',smoothingParam);
     prediction{ii} = feval(smoothFit,csfSFs);
     
@@ -23,7 +48,7 @@ for ii = 1:nFits
     fitError = fitError + sum(diff.^2);
 end
 
-% Normalize the error.
+%% Normalize the fitting error.
 fitError = sqrt(fitError/(nFits * nSFs));
 
 end
