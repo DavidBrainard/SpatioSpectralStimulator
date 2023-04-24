eefcc454170967bfeaff01b2601623e6581116f6
SeminOh <ohsemin1990@gmail.com>
2023-04-24 16:42:42 -0400

    (SEMIN) Now save combined CCSF figure and added option to plot for all subject by key press

--- fitcsf/SACC_FitCSFReadOnly.m
+++ fitcsf/SACC_FitCSFReadOnly.m
@@ -7,15 +7,16 @@
 
 % History:
 %    3/28/23   smo    - Wrote it.
+%    4/24/23   smo    - Now we save the CCSF figure and also read the
+%                       results continuosly by key press.
 
 %% Initialize.
 clear; close all;
 
 %% Set variables.
-%
-% If oneFigure set to true, we will plot all results on one figure.
-% Otherwise, we will make a separate plot per each filter.
-oneFigure = true;
+SaveFigure = true;
+PlotAllSubjects = true;
+WaitForKeyPress = true;
 
 fittingMode = 'crossValBootAcrossFmincon';
 filterOptions = {'A', 'B', 'C', 'D', 'E'};
@@ -47,84 +48,116 @@ if (ispref('SpatioSpectralStimulator','SACCAnalysis'))
         targetFilename = append(sprintf('CSF_%s',whichSub));
         fileList = dir(append(fullfile(testFiledir,targetFilename),'*'));
         
-        % Check the number of available figures per subject.
+        % Check if each subject has CSF plots for all filters.
         nFiles = 0;
-        for ff = 1:length(fileList)
-            fileNameTemp = string(fileList(ff).name);
-            nFiles = nFiles + contains(fileNameTemp,append(whichSub,'_'));
+        for ff = 1:length(filterOptions)
+            fileNameTemp = sprintf('%s_%s.%s',targetFilename,filterOptions{ff},imgFormat);
+            for ll = 1:length(fileList)
+                nFiles = nFiles + contains(fileNameTemp,fileList(ll).name);
+            end
         end
         
         % Save if the subject has all available figures.
-        if (nFiles == 5)
+        if (nFiles == length(filterOptions))
             subjectAvailable{end+1} = whichSub;
         end
     end
 end
 
 %% Prompt which subject result to read.
-while 1
-    inputMessage = sprintf('\t Choose a subject to see the results: \n \t [%s] \n',strjoin(subjectAvailable));
-    whichSub = input(inputMessage, 's');
-    
-    if ismember(whichSub, subjectAvailable)
-        break
+%
+% If we are not plotting all subjects, it will ask you which subject result
+% to plot.
+if (~PlotAllSubjects)
+    while 1
+        inputMessage = sprintf('\t Choose a subject to see the results: \n \t [%s] \n',strjoin(subjectAvailable));
+        whichSub = input(inputMessage, 's');
+        
+        if ismember(whichSub, subjectAvailable)
+            break
+        end
+        
+        disp('Choose a number within the numbers displaying!');
     end
     
-    disp('Choose a number within the numbers displaying!');
+    fprintf('Subject (%s) results will be displaying...\n',whichSub);
 end
-
-fprintf('Subject (%s) results will be displaying...\n',whichSub);
 
 %% Display the results.
-testFiledir = getpref('SpatioSpectralStimulator','SACCAnalysis');
-testFiledir = fullfile(testFiledir,whichSub,'CSF');
-
-% If we make only one plot to have all results.
-if (oneFigure)
-    figure;
-    set(gcf,'position',figurePosition);
+if (PlotAllSubjects)
+    nSubjects = length(subjectAvailable);
+else
+    nSubjects = 1;
 end
 
-% Make a loop to load all results of the filters.
-for ff = 1:length(filterOptions)
-    % Draw everything on one figure or separate.
-    if (oneFigure)
-        subplot(2,3,ff);
-    else
-        figure;
-        set(gcf,'position',figurePosition);
+for ss = 1:nSubjects
+    % Choose subject to test if running for all.
+    if (PlotAllSubjects)
+        whichSub = subjectAvailable{ss};
     end
     
-    % Get the filename to plot.
-    testFilename = fullfile(testFiledir,...
-        sprintf('CSF_%s_%s.%s',whichSub,filterOptions{ff},imgFormat));
-    image = imread(testFilename);
+    % Get file location.
+    testFiledir = getpref('SpatioSpectralStimulator','SACCAnalysis');
+    testFiledir = fullfile(testFiledir,whichSub,'CSF');
     
-    % Resize the image if you want.
-    imgMagnifyIndex = 1;
-    image = imresize(image,imgMagnifyIndex);
-    [imgSizeHorz, imgSizeVert, ~] = size(image);
+    % Make a figure window.
+    figure;
+    set(gcf,'position',figurePosition);
     
-    % Display the image.
-    % Crop image to remove the legend which is outside the plot.
-    if (oneFigure)
+    % Make a loop to load all results of the filters.
+    for ff = 1:length(filterOptions)
+        % Make a subplot for each filter.
+        subplot(2,3,ff);
+        
+        % Get the filename to plot.
+        testFilename = fullfile(testFiledir,...
+            sprintf('CSF_%s_%s.%s',whichSub,filterOptions{ff},imgFormat));
+        image = imread(testFilename);
+        
+        % Resize the image if you want.
+        imgMagnifyIndex = 1;
+        image = imresize(image,imgMagnifyIndex);
+        [imgSizeHorz, imgSizeVert, ~] = size(image);
+        
+        % Display the image.
+        % Crop image to remove the legend which is outside the plot.
         imageCropped = imcrop(image,[0 0 imgSizeVert-625 imgSizeHorz]);
         imshow(imageCropped);
-    else
-        imshow(image);
-    end
-    
-    % Add title per each image.
-    if (oneFigure)
+        
+        % Add title per each image.
         title(sprintf('Filter %s',filterOptions{ff}),'fontsize',20);
-    end
-    
-    % Add legend.
-    if (oneFigure)
+        
+        % Add legend.
         if ff == length(filterOptions)
             subplot(2,3,ff+1);
             imageLegend = imcrop(image,[1040 90 525 150]);
             imshow(imageLegend);
         end
+    end
+    
+    % Save the results.
+    if (SaveFigure)
+        if (ispref('SpatioSpectralStimulator','SACCAnalysis'))
+            testFiledir = getpref('SpatioSpectralStimulator','SACCAnalysis');
+            testFiledir = fullfile(testFiledir,whichSub,'CSF');
+            testFilename = fullfile(testFiledir,...
+                sprintf('CSF_%s_AllFiltersSmooth.%s',whichSub,imgFormat));
+            saveas(gcf, testFilename);
+            fprintf('\t CSF plot has been successfully saved! \n');
+        end
+    end
+    
+    % Key press to draw next plot.
+    if (PlotAllSubjects)
+        % Show the progess.
+        fprintf('\t Press a key to draw next plot! - (%d/%d) \n',ss,nSubjects);
+        
+        % Wait for the key press to plot next one.
+        if (WaitForKeyPress)
+            pause;
+        end
+        
+        % Close the figure.
+        close all;
     end
 end
