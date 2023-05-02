2f54305b47bb5d3c3fdcde5d315d91e82ecba4a8
SeminOh <ohsemin1990@gmail.com>
2023-05-02 12:29:37 -0400

    (SEMIN) Now making a total of 3 plots for AUC analysis

--- experiment/SACC_AUCAnalyze.m
+++ experiment/SACC_AUCAnalyze.m
@@ -26,12 +26,15 @@ T = sortrows(T,sortingCriteria);
 filterOptions = {'A', 'B', 'C', 'D', 'E'};
 nFilters = length(filterOptions);
 
+% Subjects.
+nSubjects = length(unique(T.Subject));
+
 % Count the number of subjects per each filter. This part will be
 % elaborated later on. For now, it works fine.
 nAUCPerSub = 0;
 for nn = 1:size(T,1)
-    if (T.Filter{nn} == filterOptions{1}) 
-    nAUCPerSub = nAUCPerSub + 1;
+    if (T.Filter{nn} == filterOptions{1})
+        nAUCPerSub = nAUCPerSub + 1;
     end
 end
 
@@ -45,8 +48,8 @@ meanAUC = mean(AUCPerFilter,1);
 stdAUC = std(AUCPerFilter,1);
 
 %% Plot the results.
-% 
-% Bar graph.
+%
+% 1) Mean AUC results - bar graph.
 figure; hold on;
 xAxisBar = [1:length(meanAUC)];
 bar(xAxisBar,meanAUC);
@@ -56,4 +59,37 @@ xlabel('Filter','Fontsize',15);
 ylabel('Mean AUC','Fontsize',15);
 errorbar(xAxisBar,meanAUC,stdAUC,'k','linewidth',1);
 legend(sprintf('Mean AUC (N=%d)',nAUCPerSub),'FontSize',13);
-title('Mean AUC over the Filters','Fontsize',15);
+title('Mean AUC results over the Filters','Fontsize',15);
+
+% 2) AUC over the subjects - bar graph.
+indvFig = figure; clf; hold on;
+figPosition = [100 100 1500 1000];
+set(indvFig, 'position', figPosition);
+subjectNames = T.Subject(1:nAUCPerSub);
+for ff = 1:nFilters
+    subplot(2,3,ff);
+    xAxisBar = [1:size(AUCPerFilter,1)];
+    bar(xAxisBar,AUCPerFilter(:,ff));
+    xticks(xAxisBar);
+    xticklabels(subjectNames);
+    xlabel('Subject','Fontsize',15);
+    ylabel('AUC','Fontsize',15);
+    title(sprintf('Filter = %s',filterOptions{ff}),'fontsize',15);
+end
+sgtitle('AUC results of individual subjects per each Filter','Fontsize',20);
+
+% 3) AUC results over the filters - line graph.
+figure; clf; hold on;
+xAxisBar = [1:nFilters];
+xticks(xAxisBar);
+xticklabels(filterOptions);
+xlabel('Filter','Fontsize',15);
+ylabel('AUC','Fontsize',15);
+for ss = 1:nSubjects
+    plot(xAxisBar,AUCPerFilter(ss,:),'o-','linewidth',0.7);
+end
+plot(xAxisBar,meanAUC,'k+-','color',[0 0 0 0.4],'linewidth',7);
+legendLinePlot = append('Sub ',subjectNames);
+legendLinePlot{end+1} = 'Mean';
+legend(legendLinePlot,'location','northeastoutside');
+title('AUC results over the filters per each subject','fontsize',15);
