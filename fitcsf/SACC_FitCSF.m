cb12ecb5c81e022a2235803ad3829551e1af7179
SeminOh <ohsemin1990@gmail.com>
2023-05-04 17:20:19 -0400

    (SEMIN) Axis update on AUC plot

--- fitcsf/SACC_FitCSF.m
+++ fitcsf/SACC_FitCSF.m
@@ -753,7 +753,7 @@ for ss = 1:nSubjects
                 yaxisRange = log10([10, 100, 200, 300, 400, 500, 600]);
                 ylim(log10([1 600]));
                 yticks(yaxisRange);
-                yticklabels(yaxisRange);
+                ytickformat('%.2f');
                 title(sprintf('CSF curve - Sub %s / Filter %s',subjectName,filterOptions{ff}),'fontsize',15);
                 subtitle('Fitting was done on log CS - linear SF space');
                 
@@ -901,7 +901,7 @@ for ss = 1:nSubjects
             yaxisRange = log10([10, 100, 200, 300, 400, 500, 600]);
             ylim(log10([1 600]));
             yticks(yaxisRange);
-            yticklabels(yaxisRange);
+            ytickformat('%.2f');
             
             title(sprintf('CSF curve - Sub %s',subjectName),'fontsize',15);
             
