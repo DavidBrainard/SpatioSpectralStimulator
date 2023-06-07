00c6fae5583685ae6ce1bc0b08ff1ba0873bebd5
SeminOh <ohsemin1990@gmail.com>
2023-06-07 17:36:15 -0400

    (SEMIN) Now we are plotting with luminance values

--- experiment/SACC_IsoLuminanceAnalyze.m
+++ experiment/SACC_IsoLuminanceAnalyze.m
@@ -3,10 +3,11 @@
 % This is for analyzing the iso-luminance determination results.
 %
 % See also:
-%    SACC_IsoLuminanceDetermination
+%    SACC_IsoLuminanceDetermination, LightConversions, SPDToLuminance.
 
 % History:
-%    12/5/22   smo    - Started on it.
+%    12/05/22   smo    - Started on it.
+%    06/07/23   smp    - Now we plot the results using the luminance value.
 
 %% Initialize.
 clear; close all;
@@ -64,12 +65,12 @@ end
 figure; clf; hold on;
 fontSize = 15;
 xlabel('Subject name','fontsize',fontSize);
-ylabel('Output (no unit)','fontsize',fontSize);
+ylabel('Luminance (cd/m2)','fontsize',fontSize);
 xlim([1.5 str2double(numSubjects{end})])
-ylim([0 1.4]);
+% ylim([0 1.4]);
 xticks([str2double(numSubjects{1}) : 1 : str2double(numSubjects{end})]);
 title('Iso-luminance determination results','fontsize',fontSize);
-subtitle('This is the raw results');
+subtitle('Raw results');
 
 nSubjects = length(numSubjects);
 for ss = 1:nSubjects
@@ -81,26 +82,46 @@ for ss = 1:nSubjects
         redInputSettings = allData{ss}.results;
     end
     
-    % Linear fitting to get output settings of the matching results (no unit).
+% We used to do use arb power with no unit, but now we have updated to use
+% the luminance value as below.
+%     % Linear fitting to get output settings of the matching results (no unit).
+%     x = allCalData{ss}.data.redIntensity;
+%     y = allCalData{ss}.dataBC.xyzRedBC(2,:);
+%     p = polyfit(x, y, 1);
+%     redOutputSettings = polyval(p, redInputSettings);
+%     greenOutputSetting = allCalData{ss}.dataBC.xyzGreenBC(2);
+     
+    % Linear fitting to get luminance settings (cd/m2).
     x = allCalData{ss}.data.redIntensity;
-    y = allCalData{ss}.dataBC.xyzRedBC(2,:);
+    nReds = size(allCalData{ss}.dataBC.spdRedBC,2);
+    for rr = 1:nReds
+        y(rr) = SPDToLuminance(allCalData{ss}.dataBC.spdRedBC(:,rr));
+    end 
     p = polyfit(x, y, 1);
-    greenOutputSetting = allCalData{ss}.dataBC.xyzGreenBC(2);
-    redOutputSettings = polyval(p, redInputSettings);
+    redLuminance = polyval(p, redInputSettings);
+    greenLuminance = SPDToLuminance(allCalData{ss}.dataBC.spdGreenBC);
     
     % Calculate mean and std of the output settings.
-    meanRedOutputSettings = mean(redOutputSettings);
-    stdRedOutputSettings = std(redOutputSettings);
+    meanRedLuminance = mean(redLuminance);
+    stdRedLuminance = std(redLuminance);
     
     % Plot raw data (Red).
-    plot(ss*ones(1,length(redOutputSettings)), redOutputSettings,'ko');
+    plot(ss*ones(1,length(redLuminance)), redLuminance,'ko');
     
     % Plot the reference (Green).
-    plot(ss, greenOutputSetting,'go','markersize',9,'markeredgecolor','k','markerfacecolor','g');
+    plot(ss, greenLuminance,'go','markersize',9,'markeredgecolor','k','markerfacecolor','g');
     
     % Plot mean and its standard deviation (Red).
-    plot(ss, meanRedOutputSettings,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
-    errorbar(ss, meanRedOutputSettings, stdRedOutputSettings, 'r', 'linewidth', 1);
+    plot(ss, meanRedLuminance,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
+    errorbar(ss, meanRedLuminance, stdRedLuminance, 'r', 'linewidth', 1);
+    
+    % Save out some results for plotting it again after normalization.
+    redLuminanceStruct{ss} = redLuminance;
+    greenLuminanceStruct{ss} = greenLuminance;
+    greenLuminanceALL(ss) = greenLuminance;
+    
+    % Print out the progress.
+    fprintf('Progress - subject (%d/%d) \n', ss, nSubjects);
 end
 xlim([0,nSubjects+1]);
 xticks([1:1:nSubjects]);
@@ -122,47 +143,34 @@ end
 figure; clf; hold on;
 fontSize = 15;
 xlabel('Subject name','fontsize',fontSize);
-ylabel('Output (no unit)','fontsize',fontSize);
+ylabel('Luminance (cd/m2)','fontsize',fontSize);
 xlim([1.5 str2double(numSubjects{end})])
-ylim([0 1.4]);
 xticks([str2double(numSubjects{1}) : 1 : str2double(numSubjects{end})]);
 title('Iso-luminance determination results','fontsize',fontSize);
-subtitle('This is the normalized results for reference(green) to be 1');
+
+meanGreenLuminance = mean(greenLuminanceALL);
+subtitle(sprintf('This is normalized results for green to be its mean luminance = %.2f (cd/m2)',meanGreenLuminance));
 
 nSubjects = length(numSubjects);
 for ss = 1:nSubjects
-    numSubject = str2double(numSubjects{ss});
-    
-    if (numSubject == 2)
-        redInputSettings = [92 104 116 104 100 112];
-    else
-        redInputSettings = allData{ss}.results;
-    end
-    
-    % Linear fitting to get output settings of the matching results (no unit).
-    x = allCalData{ss}.data.redIntensity;
-    y = allCalData{ss}.dataBC.xyzRedBC(2,:);
-    p = polyfit(x, y, 1);
-    greenOutputSetting = allCalData{ss}.dataBC.xyzGreenBC(2);
-    redOutputSettings = polyval(p, redInputSettings);
-    
-    % Normalize it if you want.
-    greenOutputSetting = greenOutputSetting/greenOutputSetting;
-    redOutputSettings = redOutputSettings./greenOutputSetting;
+    % Normalize it.
+    luminanceNormalizationFactor = meanGreenLuminance/greenLuminanceStruct{ss};
+    greenLuminance = greenLuminanceStruct{ss}*luminanceNormalizationFactor;
+    redLuminance = redLuminanceStruct{ss}.*luminanceNormalizationFactor;
     
     % Calculate mean and std of the output settings.
-    meanRedOutputSettings = mean(redOutputSettings);
-    stdRedOutputSettings = std(redOutputSettings);
+    meanRedLuminance = mean(redLuminance);
+    stdRedLuminance = std(redLuminance);
     
     % Plot raw data (Red).
-    plot(ss*ones(1,length(redOutputSettings)), redOutputSettings,'ko');
+    plot(ss*ones(1,length(redLuminance)), redLuminance,'ko');
     
     % Plot the reference (Green).
-    plot(ss, greenOutputSetting,'go','markersize',9,'markeredgecolor','k','markerfacecolor','g');
+    plot(ss, greenLuminance,'go','markersize',9,'markeredgecolor','k','markerfacecolor','g');
     
     % Plot mean and its standard deviation (Red).
-    plot(ss, meanRedOutputSettings,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
-    errorbar(ss, meanRedOutputSettings, stdRedOutputSettings, 'r', 'linewidth', 1);
+    plot(ss, meanRedLuminance,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
+    errorbar(ss, meanRedLuminance, stdRedLuminance, 'r', 'linewidth', 1);
 end
 xlim([0,nSubjects+1]);
 xticks([1:1:nSubjects]);
