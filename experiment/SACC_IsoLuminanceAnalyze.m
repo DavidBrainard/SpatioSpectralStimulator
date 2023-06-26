2b4996c28d37e6c8173b96186171bb0690e835b4
Semin <seminoh@sas.upenn.edu>
2023-06-26 15:40:06 +0900

    (SEMIN) Iso-luminance plot added (Settings vs. luminance)

--- experiment/SACC_IsoLuminanceAnalyze.m
+++ experiment/SACC_IsoLuminanceAnalyze.m
@@ -12,47 +12,47 @@
 %% Initialize.
 clear; close all;
 
-%% Set the options. 
+%% Set the options.
 SAVETHEPLOT = true;
 
 %% Load the data.
 if (ispref('SpatioSpectralStimulator','SACCData'))
     testFiledir = getpref('SpatioSpectralStimulator','SACCData');
     testFileList = dir(testFiledir);
-    
+
     % Find available subject name.
     for tt = 1:length(testFileList)
         testFilenameList{tt}  = testFileList(tt).name;
     end
     idxSubjectName = find(str2double(testFilenameList)>0);
     subjectNameOptions = testFilenameList(idxSubjectName);
-    
+
     % Find subjects that has iso-luminance determination data.
     nSubjectName = length(subjectNameOptions);
     numSubjects = {};
     allData = {};
     allCalData = {};
-    
+
     for ss = 1:nSubjectName
         testFiledirTemp = fullfile(testFiledir,subjectNameOptions{ss},'FlickerPhotom');
         subjectNameTemp = subjectNameOptions{ss};
-        
+
         % Load the data here if available.
         if isdir(testFiledirTemp)
             testFilenameTemp = GetMostRecentFileName(testFiledirTemp,...
                 sprintf('flickerPhotom_%s',subjectNameTemp));
             data = load(testFilenameTemp);
-            
+
             % Load the calibration file used for the experiment.
             [dir filename format] = fileparts(testFilenameTemp);
             numExtract = regexp(filename,'\d+','match');
             date = sprintf('%s-%s-%s',numExtract{2},numExtract{3},numExtract{4});
-            
+
             calFiledir = fullfile(testFiledir,'CheckFlickerPhotom');
             calDataFilename = GetMostRecentFileName(calFiledir,...
                 sprintf('checkFlickerPhotom_%s',date));
             calData = load(calDataFilename);
-        
+
             % Save subject name and the data.
             numSubjects{end+1} = subjectNameTemp;
             allData{end+1} = data.data;
@@ -75,51 +75,51 @@ subtitle('Raw results');
 nSubjects = length(numSubjects);
 for ss = 1:nSubjects
     numSubject = str2double(numSubjects{ss});
-    
+
     if (numSubject == 2)
         redInputSettings = [92 104 116 104 100 112];
     else
         redInputSettings = allData{ss}.results;
     end
-    
-% We used to do use arb power with no unit, but now we have updated to use
-% the luminance value as below.
-%     % Linear fitting to get output settings of the matching results (no unit).
-%     x = allCalData{ss}.data.redIntensity;
-%     y = allCalData{ss}.dataBC.xyzRedBC(2,:);
-%     p = polyfit(x, y, 1);
-%     redOutputSettings = polyval(p, redInputSettings);
-%     greenOutputSetting = allCalData{ss}.dataBC.xyzGreenBC(2);
-     
+
+    % We used to do use arb power with no unit, but now we have updated to use
+    % the luminance value as below.
+    %     % Linear fitting to get output settings of the matching results (no unit).
+    %     x = allCalData{ss}.data.redIntensity;
+    %     y = allCalData{ss}.dataBC.xyzRedBC(2,:);
+    %     p = polyfit(x, y, 1);
+    %     redOutputSettings = polyval(p, redInputSettings);
+    %     greenOutputSetting = allCalData{ss}.dataBC.xyzGreenBC(2);
+
     % Linear fitting to get luminance settings (cd/m2).
     x = allCalData{ss}.data.redIntensity;
     nReds = size(allCalData{ss}.dataBC.spdRedBC,2);
     for rr = 1:nReds
         y(rr) = SPDToLuminance(allCalData{ss}.dataBC.spdRedBC(:,rr));
-    end 
+    end
     p = polyfit(x, y, 1);
     redLuminance = polyval(p, redInputSettings);
     greenLuminance = SPDToLuminance(allCalData{ss}.dataBC.spdGreenBC);
-    
+
     % Calculate mean and std of the output settings.
     meanRedLuminance = mean(redLuminance);
     stdRedLuminance = std(redLuminance);
-    
+
     % Plot raw data (Red).
     plot(ss*ones(1,length(redLuminance)), redLuminance,'ko');
-    
+
     % Plot the reference (Green).
     plot(ss, greenLuminance,'go','markersize',9,'markeredgecolor','k','markerfacecolor',[0.1 0.7 0.1]);
-    
+
     % Plot mean and its standard deviation (Red).
     plot(ss, meanRedLuminance,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
     errorbar(ss, meanRedLuminance, stdRedLuminance, 'r', 'linewidth', 1);
-    
+
     % Save out some results for plotting it again after normalization.
     redLuminanceStruct{ss} = redLuminance;
     greenLuminanceStruct{ss} = greenLuminance;
     greenLuminanceALL(ss) = greenLuminance;
-    
+
     % Print out the progress.
     fprintf('Progress - subject (%d/%d) \n', ss, nSubjects);
 end
@@ -157,17 +157,17 @@ for ss = 1:nSubjects
     luminanceNormalizationFactor = meanGreenLuminance/greenLuminanceStruct{ss};
     greenLuminance = greenLuminanceStruct{ss}*luminanceNormalizationFactor;
     redLuminance = redLuminanceStruct{ss}.*luminanceNormalizationFactor;
-    
+
     % Calculate mean and std of the output settings.
     meanRedLuminance = mean(redLuminance);
     stdRedLuminance = std(redLuminance);
-    
+
     % Plot raw data (Red).
     plot(ss*ones(1,length(redLuminance)), redLuminance,'ko');
-    
+
     % Plot the reference (Green).
     plot(ss, greenLuminance,'go','markersize',9,'markeredgecolor','k','markerfacecolor',[0.1 0.7 0.1]);
-    
+
     % Plot mean and its standard deviation (Red).
     plot(ss, meanRedLuminance,'ro','markersize',9,'markeredgecolor','k','markerfacecolor','r');
     errorbar(ss, meanRedLuminance, stdRedLuminance, 'r', 'linewidth', 1);
@@ -187,3 +187,18 @@ if (SAVETHEPLOT)
         disp('Iso-luminance determination result plot has been successfully saved! - (normalized)');
     end
 end
+
+%% Settings vs. Luminance.
+%
+% This part can be run after reading one of the measurement data. We will
+% elaborate it later on.
+figure; clf; hold on;
+for ss = 1:size(dataBC.spdRedBC,2)
+    luminance_Red(ss) = SPDToLuminance(dataBC.spdRedBC(:,ss));
+end
+luminance_Green = SPDToLuminance(dataBC.spdGreenBC);
+plot(data.greenIntensity, luminance_Green,'ko','markersize',11,'markerfacecolor',[0.1 0.7 0.1]);
+plot(data.redIntensity, luminance_Red,'ko','markersize',11,'markerfacecolor','r')
+xlabel('Input settings (8-bit)','fontsize',13)
+ylabel('Luminance (cd/m2)','fontsize',13);
+legend('Green', 'Red','fontsize',13,'location','southeast');
