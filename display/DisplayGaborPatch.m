d4895def9dc690032f1f51bea4c205868f030175
SeminOh <ohsemin1990@gmail.com>
2023-10-09 15:23:13 -0400

    (SEMIN) In progress displaying an image on the SACCSFA

--- display/DisplayGaborPatch.m
+++ display/DisplayGaborPatch.m
@@ -1,24 +1,40 @@
-%% Display a Gabor patch on the DLP using the Psychtoolbox
-% This code is to display Gabor patch for SACC project
+% DisplayGaborPatch. 
+%
+% This displays an image on the DLP using the Psychtoolbox. 
+%
+% History:
+%    10/09/23  smo    - Modified it.
 
-% It is useful when your computer cannot configure the target display (LED
-% screen in SACC project)
+%% Initialize.
+close all; clear;
 
-% This code can be used regardless of the configuration state as it uses
-% Psychtoolbox
+%% Load the image data.
+imageType = 'normal';
+olderDate = 5;
+if (ispref('SpatioSpectralStimulator','SACCData'))
+    testFiledir = fullfile(getpref('SpatioSpectralStimulator','SACCData'),'TestImages');
+    % We set the test file name differently over the image type either
+    % normla or high.
+    switch imageType
+        case 'normal'
+            testFilename = GetMostRecentFileName(testFiledir,'RunExpData_18_cpd_','olderDate',olderDate);
+        case 'high'
+            testFilename = GetMostRecentFileName(testFiledir,'RunExpData_high_18_cpd_','olderDate',olderDate);
+    end
+    % Load the image date here.
+    imageData = load(testFilename);
+end
 
-%% Initialize
-sca;
-close all;
-clear all;
+% Load the highest test image to test. Test images are saved in an
+% ascending order of the contrast, so we will load the last image in the
+% array.
+testImage = imageData.sceneParamsStruct.predefinedRGBImages{end};
 
-%% Load the Gabor patch image
-% Read the gaborPatch generated from 'SpectralTestcal.m'
-gaborPatch = load('testImageData1.mat'); % Load the Gabor patch image
-gaborPatchImage = gaborPatch.screenSettingsImage; % Read the image data from the struct
-gaborPatchImage = im2uint8(gaborPatchImage); % Convert the format of the image from 'double' to 'uin8'
+% Set primary channel settings as we did in the experiment.
+channelSettings = imageData.experimentParams.screenPrimarySettings;
+SetChannelSettings(channelSettings);
 
-%% PTB pre-setup
+%% PTB pre-setup.
 PsychDefaultSetup(2); 
 
 screens = Screen('Screens');    
@@ -31,12 +47,13 @@ black = BlackIndex(screenNumber);
 [screenXpixels, screenYpixels] = Screen('WindowSize', window); % Read the resolution of the display
 
 %% Display a background as plain screen
+%
 % Backgroundcolor can be set, but here it is set as the same size between
 % the background and the display window, so it will not be shown on the final result
 backgroundColor = [255 255 255]; % Range 0-255 (8 bits)
 [window, backgroundRect] = Screen('OpenWindow', screenNumber, backgroundColor, windowRect); % Size of the background = Size of the window
 
 %% Display the gabor patch as 'texture'
-gaborPatchImage_texture = Screen('MakeTexture', window, gaborPatchImage); % Set the gaborPatchImage as 'texture' in PTB
+gaborPatchImage_texture = Screen('MakeTexture', window, testImage); % Set the gaborPatchImage as 'texture' in PTB
 Screen('DrawTexture', window, gaborPatchImage_texture, [], backgroundRect);
 Screen('Flip', window);
