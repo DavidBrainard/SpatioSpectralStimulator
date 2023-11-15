3210696ce1ee146c75eb6281070ec5bcea197992
semin-oh <seminoh@sas.upenn.edu>
2023-11-15 16:01:13 -0500

    (SEMIN) Added one more para (offset) to fit sine curve

--- toolbox/analysis/FitSineWave.m
+++ toolbox/analysis/FitSineWave.m
@@ -22,12 +22,14 @@ function [params fittedWaveform] = FitSineWave(waveform,options)
 %    A0                        - Initial amplitude (default: 1).
 %    f0                        - Initial frequency (default: 1).
 %    phi0                      - Initial phase (default: 0).
+%    B0                        - Initial offset (default: 0).
 %    lb                        - Lower bounds for parameters [A, f, phi] (default: [0, 0, -pi]).
 %    ub                        - Upper bounds for parameters [A, f, phi] (default: [Inf, Inf, pi]).
 %    verbose                   - Display additional information (default: false).
 
 % History:
-%    11/09/23  smo              - Started on it
+%    11/09/23  smo             - Started on it
+%    11/15/23  smo             - Added one more parameter (offset) to fit.
 
 %% Set variables.
 arguments
@@ -35,8 +37,9 @@ arguments
     options.A0 (1,1) = 1
     options.f0 (1,1) = 1
     options.phi0 (1,1) = 0
-    options.lb (1,3) = [0, 0, -pi];
-    options.ub (1,3) = [Inf, Inf, pi];
+    options.B0 (1,1) = 0
+    options.lb (1,4) = [0, 0, -pi, -Inf];
+    options.ub (1,4) = [Inf, Inf, pi, Inf];
     options.FFT (1,1) = false
     options.verbose (1,1) = true
 end
@@ -89,28 +92,18 @@ end
 x_data = linspace(0,1,length(waveform));
 
 % Define the objective function for optimization.
-objective_function = @(params) norm(params(1)*sin(2*pi*params(2)*x_data + params(3)) - waveform);
+objective_function = @(params) norm((params(1)*sin(2*pi*params(2)*x_data + params(3))+ params(4)) - waveform);
 
 % Set the initial starting value. The fitting results are extemely
 % sensitive to the initial frequency setting f0.
-initial_guess = [options.A0, options.f0, options.phi0];
+initial_guess = [options.A0, options.f0, options.phi0, options.B0];
 
 % Call fmincon to optimize the parameters.
 optionsFmincon = optimoptions('fmincon');
-optimized_params = fmincon(objective_function, initial_guess, [], [], [], [], options.lb, options.ub, [], optionsFmincon);
-
-% Extract optimized parameters
-A_optimized = optimized_params(1);
-f_optimized = optimized_params(2);
-phi_optimized = optimized_params(3);
-
-% Save out the fitted params. We will print out these values.
-params(1) = A_optimized;
-params(2) = f_optimized;
-params(3) = phi_optimized;
+params = fmincon(objective_function, initial_guess, [], [], [], [], options.lb, options.ub, [], optionsFmincon);
 
 % Generate the fitted waveform using the optimized parameters.
-fittedWaveform = A_optimized * sin(2*pi*f_optimized*x_data + phi_optimized);
+fittedWaveform = params(1) * sin(2*pi*params(2)*x_data + params(3)) + params(4);
 
 % Plot the fitting results if you want.
 if (options.verbose)
@@ -129,9 +122,10 @@ if (options.verbose)
     
     % Display optimized parameters
     fprintf('Optimized Parameters:\n');
-    fprintf('a = %.4f\n', A_optimized);
-    fprintf('b = %.4f\n', f_optimized);
-    fprintf('c = %.4f\n', phi_optimized);
+    fprintf('A = %.4f\n', params(1));
+    fprintf('f = %.4f\n', params(2));
+    fprintf('phi = %.4f\n', params(3));
+    fprintf('B = %.4f\n', params(4));
 end
 
 end
