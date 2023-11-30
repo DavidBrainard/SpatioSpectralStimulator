2d8eb1a7b0def4a62f026ba9c671a28e844b4e6c
SeminOh <ohsemin1990@gmail.com>
2023-11-30 11:21:16 -0500

    (SEMIN) New function to capture camera image and the script has been substituted with the function

--- camera/CameraContrastRealtime.m
+++ camera/CameraContrastRealtime.m
@@ -13,13 +13,11 @@
 clear; close all; clc;
 
 %% Open camera.
-vid = OpenCamera;
+rectRatioWidth = 0.1;
+rectRatioHeight = 0.08;
+vid = OpenCamera('rectRatioHeight',rectRatioHeight,'rectRatioWidth',rectRatioWidth);
 
 %% Capture image and calculate contrast.
-%
-% Repeat this part to update the contrast calculation results on the camera
-% preview screen. It is not 100% real-time measurements, but works pretty
-% fast.
 %
 % Clear text on the camera preview. This would make sort of real-time
 % measurement by updating the numbers.
@@ -27,113 +25,54 @@ fig = gcf;
 textObjects = findall(fig,'Type','text');
 delete(textObjects);
 
-% Control exposure time. 
-% Default = 10000. Set it higher number for brighter
-% image.
-src = getselectedsource(vid);
+% Set which channel and spatial frequency to test.
+numChannel = 1;
+cyclesPerDeg = 3;
 
 % Set it differently over the channel. This value was found when we turn on
 % a single channel of combi-LED in input setting of 0.5 intensity (0-1).
-% numChannel = 5;
-switch numChannel
+exposureTimePerChannel = [27000 45000 40000 58000 21000 210000 47000 47000];
+exposureTime = exposureTimePerChannel(numChannel);
+
+% Set which viewing media to use either SACCSFA or printed target for
+% camera MTF.
+numViewingMedia = 1;
+switch numViewingMedia
     case 1
-    src.ExposureTime = 27000;
+        viewingMedia = 'SACCSFA';
     case 2
-        src.ExposureTime = 45000;
-        case 3
-        src.ExposureTime = 40000;
-        case 4
-        src.ExposureTime = 58000;
-        case 5
-        src.ExposureTime = 21000;
-        case 6
-        src.ExposureTime = 210000;
-        case 7
-        src.ExposureTime = 47000;
-        case 8
-        src.ExposureTime = 47000;    
-        otherwise
-        % Default = 10000.
-        src.ExposureTime = 10000;     
+        viewingMedia = 'Print';
 end
 
-% Save a camera image. We will calculate the contrast from still image.
-start(vid);
-image = getdata(vid);
-imagesize = size(image);
-pixelHeight = imagesize(1);
-pixelWidth = imagesize(2);
+% Get today's date.
+date = datestr(now,'yyyy-mm-dd');
 
-% Save the raw image here for saving it later.
-imageRaw = image(1:pixelHeight,1:pixelWidth);
+% We will add channel and spatial frequency in the file name.
+channel = append('Ch',num2str(numChannel));
+SF = append(num2str(cyclesPerDeg),'cpd');
 
-% Get the targeted area of the image. 
-a = (0.5-ratioHeight/2)*pixelHeight;
-b = (0.5+ratioHeight/2)*pixelHeight;
-c = (0.5-ratioWidth/2)*pixelWidth;
-d = (0.5+ratioWidth/2)*pixelWidth;
-imageCrop = image(a:b,c:d);
-[Ypixel_crop Xpixel_crop] = size(imageCrop);
+% Set the directory to save the image.
+saveImageDir = getpref('SpatioSpectralStimulator','SACCMaterials');
+saveImageDir = fullfile(saveImageDir,'Camera','ChromaticAberration',viewingMedia,date,channel);
 
-% Crop the targeted area in the image.
-imagecrop_25 = imageCrop(round(0.25*Ypixel_crop),:);
-imagecrop_50 = imageCrop(round(0.50*Ypixel_crop),:);
-imagecrop_75 = imageCrop(round(0.75*Ypixel_crop),:);
-imagecrop_avg = mean([imagecrop_25;imagecrop_50;imagecrop_75]);
+% Capture an image here and do analysis of it.
+CaptureCamera(vid,'rectRatioHeight',rectRatioHeight,'rectRatioWidth',rectRatioHeight,...
+    'exposureTime',exposureTime,'minPeakDistancePixel',minPeakDistancePixel,...
+    'saveImagedir',saveImageDir,'saveImageFilename',SF);
 
-% Calculate contrast.
-white = max(imagecrop_avg);
-black = min(imagecrop_avg);
-contrast = (white-black)/(white+black);
-fprintf('Contrast = (%.2f) \n', contrast);
-
-% Show contrast on the camera preview.
-textContrast = append('Contrast:  ',num2str(round(contrast,2)));
-text(1.05*imWidth*markerindex, 1*imHeight*markerindex, textContrast, 'Color','w');
-
-% Show spatial frequency.
+%% Test for one image.
 %
-% Set minimum peak distance here for not having multiple peaks at one peak
-% point. Followings are recommendation over different spatial frequency.
-% 
-% minPeakDistance = 5 (9, 12 ,18 cpd), 17 (6 cpd), 40 (3 cpd).
-minPeakDistance = 40;
+% Here we are not saving the image and just checking if everything works
+% fine before running the routine.
+CAPTUREONEIMAGE = false;
 
-% Show the peaks found. Visually check.
-figPeak = figure; findpeaks(double(imagecrop_50),'MinPeakDistance',minPeakDistance)
-
-% Calculate the spatial frequency here.
-[~,peakIndex] = findpeaks(double(imagecrop_50),'MinPeakDistance',minPeakDistance);
-numCycles = length(peakIndex);
-
-% Get the horizontal size of cropped image in degrees.
-pixelToInchHorizontal = 0.0367;
-pixelToInchVertical = 0.0362;
-physicalDistnaceRefInch = 370;
-cropImageHalfInch = pixelToInchHorizontal * (Xpixel_crop/2);
-sizeDegHorizontal = 2*(rad2deg(atan(cropImageHalfInch/physicalDistnaceRefInch)));
-
-% When the cropped image was 309 x 166 pixels.
-% sizeDegHorizontal = 1.7570;
-
-% Calculate the cpd here.
-cyclesPerDeg = numCycles/sizeDegHorizontal;
-fprintf('Spatial frequency = (%.1f) \n', cyclesPerDeg);
-
-% Add spatial frequency on the camera preview.
-textCyclesPerDeg = append('Spatial frequency:  ',num2str(round(cyclesPerDeg,0)));
-text(1.05*imWidth*markerindex, 1.1*imHeight*markerindex, textCyclesPerDeg,'Color','w');
-
-% Save the image if you want.
-SAVEIMAGE = false;
-if (SAVEIMAGE)
-    testfileDir = 'C:\Users\brainardlab\Desktop\0905';
-    testfileDir = fullfile(testfileDir,append('Ch',num2str(numChannel)));
-    cyclesPerDegStr = 'single_focused_';
-    fileNameRawImage = fullfile(testfileDir,append(cyclesPerDegStr,'raw_'));
-    fileNameCropImage = fullfile(testfileDir,append(cyclesPerDegStr,'crop_'));
-    dayTimeStr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
-    imwrite(imageCrop,append(fileNameCropImage,dayTimeStr,'.tiff'));
-    imwrite(imageRaw,append(fileNameRawImage,dayTimeStr,'.tiff'));
-    disp('Image has been saved successfully!');
+if (CAPTUREONEIMAGE)
+    % Set variables.
+    exposureTime = 10000;
+    minPeakDistancePixel = 40;
+    
+    % Capture image here.
+    CaptureCamera(vid,'rectRatioHeight',rectRatioHeight,'rectRatioWidth',rectRatioHeight,...
+        'exposureTime',exposureTime,'minPeakDistancePixel',minPeakDistancePixel,...
+        'saveImagedir',[],'saveImageFilename',[]);
 end
---
+++ toolbox/camera/CaptureCamera.m
@@ -0,0 +1,135 @@
+function [image] = CaptureCamera(vid,options)
+% Capture an image using a camera.
+%
+% Syntax:
+%    [image] = CaptureCamera()
+%
+% Description:
+%    This captures an image of the scene using a camera connected to the
+%    computer.
+%
+% Inputs:
+%    N/A
+%
+% Outputs:
+%    vid                      - Video object to control.
+%
+% Optional key/value pairs:
+%    dd
+%
+% See also:
+%    OpenCamera, CameraContrastRealtime
+
+% History:
+%    11/30/23       smo       - Wrote it
+
+%% Set variables.
+arguments
+    vid
+    options.exposureTime (1,1) = 10000
+    options.rectRatioHeight (1,1) = 0.1
+    options.rectRatioWidth (1,1) = 0.08
+    options.minPeakDistancePixel (1,1) = 10
+    options.saveImageDir = []
+    options.saveImageFilename (1,1)
+end
+
+%% Control exposure time.
+src = getselectedsource(vid);
+src.exposureTime = options.exposureTime;
+
+%% Save a camera image. We will calculate the contrast from still image.
+start(vid);
+image = getdata(vid);
+imageSize = size(image);
+imagePixelHeight = imageSize(1);
+imagePixelWidth = imageSize(2);
+
+% Save the raw image here for saving it later.
+rawImage = image(1:imagePixelHeight, 1:imagePixelWidth);
+
+% Get the size of the targeted area within the image.
+a = (0.5-options.rectRatioHeight/2)*imagePixelHeight;
+b = (0.5+options.rectRatioHeight/2)*imagePixelHeight;
+c = (0.5-options.rectRatioWidth/2)*imagePixelWidth;
+d = (0.5+options.rectRatioWidth/2)*imagePixelWidth;
+
+% Get the image here.
+cropImage = image(a:b,c:d);
+[Ypixel_crop Xpixel_crop] = size(cropImage);
+
+% We calculate the mean of three (25%, 50%, 75%) intensity profiles.
+imagecrop_25 = cropImage(round(0.25*Ypixel_crop),:);
+imagecrop_50 = cropImage(round(0.50*Ypixel_crop),:);
+imagecrop_75 = cropImage(round(0.75*Ypixel_crop),:);
+imagecrop_avg = mean([imagecrop_25;imagecrop_50;imagecrop_75]);
+
+% Calculate contrast from the averaged intensity profile. The contrast here
+% is simply picking the highest and the lowest for white and black,
+% responsively. This is just for quick checking. For SACC project, we
+% calculate contrasts properly for analysis.
+white = max(imagecrop_avg);
+black = min(imagecrop_avg);
+contrast = (white-black)/(white+black);
+fprintf('Contrast = (%.2f) \n', contrast);
+
+% Show contrast on the camera preview. This would make real-time ish
+% measurement.
+markerIndex = 0.5;
+vidRes = get(vid, 'VideoResolution');
+imWidth = vidRes(1);
+imHeight = vidRes(2);
+textContrast = sprintf('Contrast: %.2f',contrast);
+text(1.05*imWidth*markerIndex, 1*imHeight*markerIndex, textContrast, 'Color', 'w');
+
+%% Calculate spatial frequency of the image.
+%
+% Find the peaks in the intensity profile. We recommend to visually check
+% the number of peaks found correctly, then proceed to calculate spatial
+% frequency.
+figure;
+findpeaks(double(imagecrop_50),'MinPeakDistance',options.minPeakDistancePixel)
+
+% Calculate the spatial frequency here.
+[~,peakIndex] = findpeaks(double(imagecrop_50),'MinPeakDistance',options.minPeakDistancePixel);
+numCycles = length(peakIndex);
+
+% Get the horizontal size of cropped image in degrees. These values were
+% found from the measurement, which would not be changed for SACC project.
+% We will calculate the horizontal size in degree to compute spatial
+% frequency.
+%
+% We first calculate the degree of the half angle, then multiply by 2 to
+% get the visual angle of the scene.
+pixelToInchHorizontal = 0.0367;
+physicalDistnaceRefInch = 370;
+cropImageHalfInch = pixelToInchHorizontal * (Xpixel_crop/2);
+sizeDegHorizontal = 2*(rad2deg(atan(cropImageHalfInch/physicalDistnaceRefInch)));
+
+% Calculate spatial frequency here.
+cyclesPerDeg = numCycles/sizeDegHorizontal;
+fprintf('Spatial frequency = (%.1f) \n', cyclesPerDeg);
+
+% Add spatial frequency on the camera preview.
+textCyclesPerDeg = sprintf('Spatial frequency: %.1f',cyclesPerDeg);
+text(1.05*imWidth*markerindex, 1.1*imHeight*markerindex, textCyclesPerDeg, 'Color', 'w');
+
+% Save the image if you want.
+if ~isempty(options.saveImageDir)
+    testfileDir = options.saveImageDir;
+    
+    % Get the date for the file name.
+    dayTimeStr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
+    
+    % Save the raw image.
+    fileNameRawImage = fullfile(testfileDir,append(options.saveImageFilename,'_raw_'));
+    imwrite(rawImage,append(fileNameRawImage,dayTimeStr,'.tiff'));
+    
+    % Save the cropped image.
+    fileNameCropImage = fullfile(testfileDir,append(options.saveImageFilename,'_crop_'));
+    imwrite(cropImage,append(fileNameCropImage,dayTimeStr,'.tiff'));
+    
+    disp('Image has been saved successfully!');
+end
+
+end
